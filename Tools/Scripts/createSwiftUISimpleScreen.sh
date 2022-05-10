da9f022e0e06ec9d06f224651a42cbd2b53507d9
Stefan Ceriu <stefanc@matrix.org>
2022-05-10 15:05:56 +0300

    Added back simple template

--- Tools/Scripts/createSwiftUISimpleScreen.sh
+++ Tools/Scripts/createSwiftUISimpleScreen.sh
@@ -5,27 +5,54 @@ if [ ! $# -eq 2 ]; then
     exit 1
 fi
 
-MODULE_DIR="../ElementX/Sources/Modules/Screens"
-OUTPUT_DIR=$MODULE_DIR/$1
-SCREEN_NAME=$2
-SCREEN_VAR_NAME=`echo $SCREEN_NAME | awk '{ print tolower(substr($0, 1, 1)) substr($0, 2) }'`
-TEMPLATE_DIR=$MODULE_DIR/Templates/SimpleScreenExample/
-if [ -e $OUTPUT_DIR ]; then
-    echo "Error: Folder ${OUTPUT_DIR} already exists"
+SCREENS_DIR="../../ElementX/Sources/Screens"/$1
+UI_TESTS_DIR="../../UITests/Sources"
+UNIT_TESTS_DIR="../../UnitTests/Sources"
+
+if [ -e $SCREENS_DIR ]; then
+    echo "Error: Folder ${SCREENS_DIR} already exists"
     exit 1
 fi
 
-echo "Create folder ${OUTPUT_DIR}"
+echo "Creating ${SCREENS_DIR}"
 
-mkdir -p $OUTPUT_DIR
-cp -R $TEMPLATE_DIR $OUTPUT_DIR/
+mkdir -p $SCREENS_DIR
 
-cd $OUTPUT_DIR
-for file in $(find * -type f -print)
-do
-  echo "Building ${file/TemplateSimpleScreen/$SCREEN_NAME}..."
-  perl -p -i -e "s/TemplateSimpleScreen/"$SCREEN_NAME"/g" $file
-  perl -p -i -e "s/templateSimpleScreen/"$SCREEN_VAR_NAME"/g" $file
+echo "Copying screen files"
 
-  mv ${file} ${file/TemplateSimpleScreen/$SCREEN_NAME}
-done
+cp -R "Templates/SimpleScreenExample/ElementX/" $SCREENS_DIR/
+
+echo "Copying tests"
+
+cp -R "Templates/SimpleScreenExample/Tests/UI/" $UI_TESTS_DIR/
+
+cp -R "Templates/SimpleScreenExample/Tests/Unit" $UNIT_TESTS_DIR/
+
+SCREEN_NAME=$2
+SCREEN_VAR_NAME=`echo $SCREEN_NAME | awk '{ print tolower(substr($0, 1, 1)) substr($0, 2) }'`
+
+function rename_files {
+    for file in $(find * -type f -print)
+    do
+      perl -p -i -e "s/TemplateSimpleScreen/"$SCREEN_NAME"/g" $file
+      perl -p -i -e "s/templateSimpleScreen/"$SCREEN_VAR_NAME"/g" $file
+
+      mv ${file} ${file/TemplateSimpleScreen/$SCREEN_NAME}
+    done
+}
+
+echo "Renaming files"
+
+cd $SCREENS_DIR
+rename_files
+cd - > /dev/null
+
+cd $UI_TESTS_DIR
+rename_files
+cd - > /dev/null
+
+cd $UNIT_TESTS_DIR
+rename_files
+cd - > /dev/null
+
+echo "Done"
