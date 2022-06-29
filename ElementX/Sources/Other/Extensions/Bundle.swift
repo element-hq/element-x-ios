4422e26956510dfbe30c2c4ce8bfc43dba07539d
ismailgulek <ismailg@matrix.org>
2022-06-29 16:44:58 +0300

    #101 Make bundle compatible with tests, remove app bundle logic

--- ElementX/Sources/Other/Extensions/Bundle.swift
+++ ElementX/Sources/Other/Extensions/Bundle.swift
@@ -10,25 +10,11 @@ import Foundation
 
 public extension Bundle {
 
-    /// Returns the real app bundle.
-    /// Can also be used in app extensions.
-    static let app: Bundle = {
-        let bundle = main
-        if bundle.bundleURL.pathExtension == "appex" {
-            // Peel off two directory levels - MY_APP.app/PlugIns/MY_APP_EXTENSION.appex
-            let url = bundle.bundleURL.deletingLastPathComponent().deletingLastPathComponent()
-            if let otherBundle = Bundle(url: url) {
-                return otherBundle
-            }
-        }
-        return bundle
-    }()
-
-    /// Get an lproj language bundle from the main app bundle.
+    /// Get an lproj language bundle from the receiver bundle.
     /// - Parameter language: The language to try to load.
     /// - Returns: The lproj bundle if found otherwise nil.
-    @objc static func lprojBundle(for language: String) -> Bundle? {
-        guard let lprojURL = Bundle.app.url(forResource: language, withExtension: "lproj") else { return nil }
+    func lprojBundle(for language: String) -> Bundle? {
+        guard let lprojURL = url(forResource: language, withExtension: "lproj") else { return nil }
         return Bundle(url: lprojURL)
     }
 
