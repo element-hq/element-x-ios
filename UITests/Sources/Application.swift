0e20566dfa03bf74959a68f4ce0694cceee42fae
ismailgulek <ismailg@matrix.org>
2022-07-05 01:40:25 +0300

    #9 Add snapshot testing to the application

--- UITests/Sources/Application.swift
+++ UITests/Sources/Application.swift
@@ -7,11 +7,13 @@
 //
 
 import XCTest
+import SnapshotTesting
 
 struct Application {
     static func launch() -> XCUIApplication {
         let app = XCUIApplication()
         app.launchEnvironment = ["IS_RUNNING_UI_TESTS": "1"]
+        Bundle.elementFallbackLanguage = "en"
         app.launch()
         return app
     }
@@ -28,4 +30,41 @@ extension XCUIApplication {
         
         button.tap()
     }
+
+    /// Assert screenshot for a screen with the given identifier. Does not fail if a screenshot is newly created.
+    /// - Parameter identifier: Identifier of the UI test screen
+    func assertScreenshot(_ identifier: UITestScreenIdentifier) {
+        let failure = verifySnapshot(
+            matching: self,
+            as: .image,
+            named: identifier.rawValue,
+            testName: testName)
+
+        if let failure = failure,
+           !failure.contains("No reference was found on disk."),
+           !failure.contains("to test against the newly-recorded snapshot") {
+            XCTFail(failure)
+        }
+    }
+
+    private var testName: String {
+        languageCode + "-" + regionCode + "-" + deviceName
+    }
+
+    private var deviceName: String {
+        UIDevice.current.name
+    }
+
+    private var languageCode: String {
+        Locale.current.languageCode ?? ""
+    }
+
+    private var regionCode: String {
+        Locale.current.regionCode ?? ""
+    }
+}
+
+extension Snapshotting where Value == XCUIElement, Format == UIImage {
+    public static let image: Snapshotting =
+    Snapshotting<UIImage, UIImage>.image.pullback { element in element.screenshot().image }
 }
