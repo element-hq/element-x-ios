132f27a6e3a6fb4918690a74031d449fc700c7e7
ismailgulek <ismailg@matrix.org>
2022-07-05 01:47:31 +0300

    #9 fix swipe gestures on iPad

--- UITests/Sources/SplashScreenUITests.swift
+++ UITests/Sources/SplashScreenUITests.swift
@@ -43,21 +43,21 @@ class SplashScreenUITests: XCTestCase {
         XCTAssertFalse(hiddenPageTitleText.isHittable, "The hidden page of the carousel should be offscreen.")
         
         // When swiping to the next screen.
-        page1TitleText.swipeLeft()
+        page1TitleText.swipeLeft(velocity: .fast)
         
         // Then the second screen should be shown.
         XCTAssertFalse(page1TitleText.isHittable, "The title from the first page of the carousel should be offscreen.")
         XCTAssertTrue(page2TitleText.isHittable, "The title from the second page of the carousel should be onscreen.")
         
         // When swiping back to the previous screen.
-        page2TitleText.swipeRight()
+        page2TitleText.swipeRight(velocity: .fast)
         
         // Then the first screen should be shown again.
         XCTAssertTrue(page1TitleText.isHittable, "The title from the first page of the carousel should be onscreen.")
         XCTAssertFalse(page2TitleText.isHittable, "The title from the second page of the carousel should be offscreen.")
         
         // When swiping back to the previous screen.
-        page1TitleText.swipeRight()
+        page1TitleText.swipeRight(velocity: .fast)
         
         // Then the screen shouldn't change and the hidden screen should be ignored.
         XCTAssertTrue(page1TitleText.isHittable, "The title from the first page of the carousel should be still be onscreen.")
