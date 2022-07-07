88b66d7bd89cc7c7cab1f394f099ce637508ea9b
ismailgulek <ismailg@matrix.org>
2022-07-07 17:50:58 +0300

    Update boot simulator script

--- Tools/Scripts/bootTestSimulator.py
+++ Tools/Scripts/bootTestSimulator.py
@@ -4,24 +4,44 @@ import os
 import subprocess
 import sys
 import json
+import argparse
 
-if len(sys.argv) != 2:
-    print('Usage: ./bootTestSimulator.py \'Simulator Name\'')
+RUNTIME_PREFIX = 'com.apple.CoreSimulator.SimRuntime.'
+
+def device_name(device):
+    return device['name']
+def runtime_name(runtime):
+    return runtime.replace(RUNTIME_PREFIX, '').replace('-', '.')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str, help='Simulator name (like \'iPhone 13 Pro Max\')', required=True)
+parser.add_argument('--version', type=str, default='iOS.15.5', help='OS version (defaults to \'iOS.15.5\')', required=False)
+
+args = vars(parser.parse_args())
+
+simulator_name = args['name']
+os_version = args['version'].replace('.', '-')
+
+runtimes_map = subprocess.check_output("/usr/bin/xcrun simctl list --json devices available", shell=True)
+runtime = RUNTIME_PREFIX + os_version
+json_object = json.loads(runtimes_map)
+if runtime in json_object['devices']:
+    devices = json_object['devices'][runtime]
+
+    device_found=False
+    for device in devices:
+        if device_name(device) == simulator_name:
+            UDID=device['udid']
+            print("Found device UDID: " + UDID)
+            os.system("/usr/bin/xcrun simctl boot '" + UDID + "' > /dev/null 2>&1")
+            os.system("/usr/bin/xcrun simctl status_bar '" + UDID + "' override --time 'test' --dataNetwork 'wifi' --wifiMode 'active' --wifiBars 3 --cellularMode 'active' --cellularBars 4 --batteryState 'charged' --batteryLevel 100 > /dev/null 2>&1")
+            print("Simulator booted and status bar overriden")
+            device_found=True
+            break
+    
+    if device_found == False:
+        print("Device could not be found. \n\nAvailable devices: " + ', '.join(map(device_name, devices)))
+        exit(1)
+else:
+    print("Runtime could not be found. \n\nAvailable runtimes: " + ', '.join(map(runtime_name, json_object['devices'].keys())))
     exit(1)
-
-simulator_name=sys.argv[1]
-
-os_version='15-5'
-runtimes_map=subprocess.check_output("/usr/bin/xcrun simctl list --json devices available", shell=True)
-runtime='com.apple.CoreSimulator.SimRuntime.iOS-' + os_version
-json_object=json.loads(runtimes_map)
-devices=json_object['devices'][runtime]
-
-for device in devices:
-    if device['name'] == simulator_name:
-        UDID=device['udid']
-        print("Found device UDID: " + UDID)
-        os.system("/usr/bin/xcrun simctl boot '" + UDID + "' > /dev/null 2>&1")
-        os.system("/usr/bin/xcrun simctl status_bar '" + UDID + "' override --time '2007-01-09T08:41:00+01:00' --dataNetwork 'wifi' --wifiMode 'active' --wifiBars 3 --cellularMode 'active' --cellularBars 4 --batteryState 'charged' --batteryLevel 100 > /dev/null 2>&1")
-        print("Simulator booted and status bar overriden")
-        break
