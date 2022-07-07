c96eb069f15ce8ff9b218b7cb74406712a5be489
ismailgulek <ismailg@matrix.org>
2022-07-07 18:18:48 +0300

    Update simulator overridden time

--- Tools/Scripts/bootTestSimulator.py
+++ Tools/Scripts/bootTestSimulator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python2.7
 
+import datetime
 import os
 import subprocess
 import sys
 import json
 import argparse
+import pytz
 
 RUNTIME_PREFIX = 'com.apple.CoreSimulator.SimRuntime.'
 
@@ -12,6 +14,22 @@ def device_name(device):
     return device['name']
 def runtime_name(runtime):
     return runtime.replace(RUNTIME_PREFIX, '').replace('-', '.')
+def utc_offset():
+    utc_offset_string = subprocess.check_output("date +%Z", shell=True).strip()
+    hours = 0
+    minutes = 0
+    parts = utc_offset_string.split(":")
+    if len(parts) > 0:
+        hours = float(parts[0])
+    if len(parts) > 1:
+        minutes = float(parts[1])/60
+    return hours + minutes
+def utc_offset_string():
+    result = subprocess.check_output("date +%Z", shell=True).strip()
+    if len(result.split(":")) > 1:
+        return result
+    else:
+        return result + ":00"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', type=str, help='Simulator name (like \'iPhone 13 Pro Max\')', required=True)
@@ -33,8 +51,16 @@ if runtime in json_object['devices']:
         if device_name(device) == simulator_name:
             UDID=device['udid']
             print("Found device UDID: " + UDID)
+
+            timezone_addition = 1 # can't explain this
+            delta = timezone_addition - utc_offset()
+            desired_time = datetime.datetime(2007, 1, 9, 9, 41, 0, tzinfo=pytz.utc)
+            final_time = desired_time + datetime.timedelta(hours=delta)
+            overridden_time = final_time.isoformat() + utc_offset_string()
+            print("Will override simulator with time: " + overridden_time)
+
             os.system("/usr/bin/xcrun simctl boot '" + UDID + "' > /dev/null 2>&1")
-            os.system("/usr/bin/xcrun simctl status_bar '" + UDID + "' override --time 'test' --dataNetwork 'wifi' --wifiMode 'active' --wifiBars 3 --cellularMode 'active' --cellularBars 4 --batteryState 'charged' --batteryLevel 100 > /dev/null 2>&1")
+            os.system("/usr/bin/xcrun simctl status_bar '" + UDID + "' override --time '" + overridden_time + "' --dataNetwork 'wifi' --wifiMode 'active' --wifiBars 3 --cellularMode 'active' --cellularBars 4 --batteryState 'charged' --batteryLevel 100 > /dev/null 2>&1")
             print("Simulator booted and status bar overriden")
             device_found=True
             break
