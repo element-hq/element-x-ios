96a06586a46e86283f0e3622750767b3db1f57fd
Doug <douglase@element.io>
2022-08-09 17:43:33 +0100

    Add default header in IDETemplateMacros.plist.

---
+++ Tools/XcodeGen/postGenCommand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is invoked by xcodegen for running post commands
+
+# Move file header template in project shared data folder
+cp IDETemplateMacros.plist ../../ElementX.xcodeproj/xcshareddata/
