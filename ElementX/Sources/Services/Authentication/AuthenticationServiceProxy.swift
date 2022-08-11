00b08abba79bf4d1b92c6f0a2681be71e9ea32c6
Hugh Nimmo-Smith <hughns@element.io>
2022-08-11 11:19:43 +0100

    Use unstable OIDC scope prefixes

--- ElementX/Sources/Services/Authentication/AuthenticationServiceProxy.swift
+++ ElementX/Sources/Services/Authentication/AuthenticationServiceProxy.swift
@@ -73,7 +73,7 @@ class AuthenticationServiceProxy: AuthenticationServiceProxyProtocol {
             let registationResponse = try await oidcService.registerClient(metadata: configuration)
             let authResponse = try await oidcService.presentWebAuthentication(metadata: configuration,
                                                                               clientID: registationResponse.clientID,
-                                                                              scope: "openid urn:matrix:api:* urn:matrix:device:\(deviceID)",
+                                                                              scope: "openid urn:matrix:org.matrix.msc2967.client:api:* urn:matrix:org.matrix.msc2967.client:device:\(deviceID)",
                                                                               userAgent: userAgent)
             let tokenResponse = try await oidcService.redeemCodeForTokens(authResponse: authResponse)
             
