21d7ff3fcef45f01ca4f854201154e4f61d60503
Stefan Ceriu <stefanc@matrix.org>
2022-10-17 17:05:13 +0300

    Avoid installing bundler in system directories

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,12 +4,19 @@
 # a nonzero exit code.
 set -e
 
-# move to the project root
+# Prevent installing dependencies in system directories
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+
+# Move to the project root
 cd ..
 
 brew bundle --file=XcodeCloudBrewfile
 
-gem install bundler
+gem install bundler --install-dir $GEM_HOME
+
 bundle config path vendor/bundle
 bundle install --jobs 4 --retry 3
 
