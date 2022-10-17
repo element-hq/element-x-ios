96e3f22cb87dcd46a7b12cc3c252d6a3d1734006
Stefan Ceriu <stefanc@matrix.org>
2022-10-17 14:59:23 +0300

    Add nightly build configuration

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Set the -e flag to stop running the script in case a command returns
+# a nonzero exit code.
+set -e
+
+brew bundle
+
+bundle config path vendor/bundle
+bundle install --jobs 4 --retry 3
+
+if [ "$CI_WORKFLOW" = "Nightly" ]; then
+    bundle exec fastlane config_nightly
+elif [ "$CI_WORKFLOW" = "Release" ]; then
+    echo "Build release"
+fi
