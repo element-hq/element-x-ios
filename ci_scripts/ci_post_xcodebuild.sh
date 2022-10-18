43b80a6ea7c4f02295d5bc28059593c386a0ef29
Stefan Ceriu <stefanc@matrix.org>
2022-10-18 17:44:22 +0300

    Install gems on the post xcodebuild action

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,8 +4,24 @@
 # a nonzero exit code.
 set -e
 
+# Prevent installing dependencies in system directories
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+
 # Move to the project root
 cd ..
+
+gem install bundler --install-dir $GEM_HOME
+
+bundle config path vendor/bundle
+bundle install --jobs 4 --retry 3
+
+if [ "$CI_WORKFLOW" = "Release" ]; then
+    bundle exec fastlane release_to_github
+    bundle exec fastlane prepare_next_release
+fi
 
 if [[ -n $CI_ARCHIVE_PATH ]];
 then
