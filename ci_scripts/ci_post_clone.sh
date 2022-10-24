6bd04ae55ac443c789302207181a266d96f37232
Stefan Ceriu <stefanc@matrix.org>
2022-10-24 12:37:00 +0300

    Cleanup ci scripts, moved most to ci_common

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,36 +1,11 @@
 #!/bin/sh
 
-# Set the -e flag to stop running the script in case a command returns
-# a nonzero exit code.
-set -e
-
-if [ "$CI_WORKFLOW" != "Nightly" ]; then
-    # We only need to run post clone steps for nightlies
-    exit 0
-fi
-
-# Prevent installing dependencies in system directories
-echo 'export GEM_HOME=$HOME/.gem' >>~/.zshrc
-echo 'export PATH=$GEM_HOME/bin:$PATH' >>~/.zshrc
-echo 'export PATH="/usr/local/opt/ruby@2.7/bin:$PATH"' >> ~/.zshrc
-
-export GEM_HOME=$HOME/.gem
-export PATH=$GEM_HOME/bin:$PATH
-export PATH="/usr/local/opt/ruby@2.7/bin:$PATH"
-
-# Move to the project root
-cd ..
-
-brew bundle --file=XcodeCloudBrewfile
-
-# Things don't work well on the default ruby version
-brew install "ruby@2.7"
-
-gem install bundler
-
-bundle config path vendor/bundle
-bundle install --jobs 4 --retry 3
+source ci_common.sh
 
 if [ "$CI_WORKFLOW" = "Nightly" ]; then
+    setup_environment()
+
+    install_brew_dependencies()
+
     bundle exec fastlane config_nightly
 fi
--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,38 +1,13 @@
 #!/bin/sh
 
-# Set the -e flag to stop running the script in case a command returns
-# a nonzero exit code.
-set -e
-
-# Prevent installing dependencies in system directories
-echo 'export GEM_HOME=$HOME/.gem' >>~/.zshrc
-echo 'export PATH=$GEM_HOME/bin:$PATH' >>~/.zshrc
-echo 'export PATH="/usr/local/opt/ruby@2.7/bin:$PATH"' >> ~/.zshrc
-
-export GEM_HOME=$HOME/.gem
-export PATH=$GEM_HOME/bin:$PATH
-export PATH="/usr/local/opt/ruby@2.7/bin:$PATH"
-
-# Move to the project root
-cd ..
-
-# Things don't work well on the default ruby version. 
-# We don't need all the other homebrew dependencies
-brew install "ruby@2.7"
-
-gem install bundler
-
-bundle config path vendor/bundle
-bundle install --jobs 4 --retry 3
+setup_environment()
 
 if [ "$CI_WORKFLOW" = "Release" ]; then
-    pip3 install -r requirements.txt # Install towncrier for generating changelogs
+    install_python_dependencies()
 
     bundle exec fastlane release_to_github
     bundle exec fastlane prepare_next_release
 fi
 
-if [[ -n $CI_ARCHIVE_PATH ]];
-then
-    bundle exec fastlane upload_dsyms_to_sentry dsym_path:"$CI_ARCHIVE_PATH/dSYMs"
-fi
+# Upload dsyms no matter the workflow
+bundle exec fastlane upload_dsyms_to_sentry dsym_path:"$CI_ARCHIVE_PATH/dSYMs"
