2ea7357415c35f868f8b098b7c80bce0b05ce669
ismailgulek <ismailg@matrix.org>
2022-10-29 14:42:29 +0300

    Resources for notifications

--- ElementX/Sources/Generated/Strings+Untranslated.swift
+++ ElementX/Sources/Generated/Strings+Untranslated.swift
@@ -24,6 +24,8 @@ extension ElementL10n {
   public static let loginMobileDevice = ElementL10n.tr("Untranslated", "login_mobile_device")
   /// Tablet
   public static let loginTabletDevice = ElementL10n.tr("Untranslated", "login_tablet_device")
+  /// Notification
+  public static let notification = ElementL10n.tr("Untranslated", "Notification")
   /// Failed creating the permalink
   public static let roomTimelinePermalinkCreationFailure = ElementL10n.tr("Untranslated", "room_timeline_permalink_creation_failure")
   /// Replying to %@
