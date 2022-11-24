56b57bee2c7078bb11ebc7c37d10d7e278e041fe
Stefan Ceriu <stefanc@matrix.org>
2022-11-24 14:54:52 +0200

    Revert tracing configuration

--- ElementX/Sources/Application/AppCoordinator.swift
+++ ElementX/Sources/Application/AppCoordinator.swift
@@ -87,19 +87,8 @@ class AppCoordinator: AppCoordinatorProtocol {
         // This exposes the full Rust side tracing subscriber filter for more flexibility.
         // We can filter by level, crate and even file. See more details here:
         // https://docs.rs/tracing-subscriber/0.2.7/tracing_subscriber/filter/struct.EnvFilter.html#examples
-        let filter = ["warn",
-                      "hyper=warn",
-                      "sled=warn",
-                      "matrix_sdk_sled=warn",
-//                      "matrix_sdk::http_client=trace"
-//                      "matrix_sdk_ffi::uniffi_api=warn",
-//                      "matrix_sdk_ffi=warn",
-//                      "matrix_sdk::sliding_sync=warn",
-//                      "matrix_sdk_base::sliding_sync=warn"
-        ].joined(separator: ",")
-        setupTracing(filter: filter)
-        
-        loggerConfiguration.logLevel = .verbose
+        setupTracing(filter: "warn,hyper=warn,sled=warn,matrix_sdk_sled=warn")
+        loggerConfiguration.logLevel = .debug
         #else
         setupTracing(filter: "info,hyper=warn,sled=warn,matrix_sdk_sled=warn")
         loggerConfiguration.logLevel = .info
