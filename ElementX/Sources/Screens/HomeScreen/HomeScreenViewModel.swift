1664ee6d7604af681487fdb6aee5f9df4279652d
Stefan Ceriu <stefanc@matrix.org>
2022-12-22 10:56:35 +0200

    Cleanup room list state computations

--- ElementX/Sources/Screens/HomeScreen/HomeScreenViewModel.swift
+++ ElementX/Sources/Screens/HomeScreen/HomeScreenViewModel.swift
@@ -79,13 +79,12 @@ class HomeScreenViewModel: HomeScreenViewModelType, HomeScreenViewModelProtocol 
                                   visibleRoomsSummaryProvider.roomListPublisher)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] state, totalCount, rooms in
-                if state != .live {
-                    if totalCount == 0 || rooms.count != totalCount {
-                        self?.state.roomListMode = .skeletons
-                    } else {
-                        self?.state.roomListMode = .rooms
-                    }
-                } else if totalCount == 0 { // Empty state
+                let isLoadingData = state != .live && (totalCount == 0 || rooms.count != totalCount)
+                let hasNoRooms = state == .live && totalCount == 0
+                
+                if isLoadingData {
+                    self?.state.roomListMode = .skeletons
+                } else if hasNoRooms {
                     self?.state.roomListMode = .skeletons
                 } else {
                     self?.state.roomListMode = .rooms
