f7ddaacb64bf65571c0604a4be6977d45e29c2eb
Doug <6060466+pixlwave@users.noreply.github.com>
2023-01-17 12:26:07 +0000

    Break out DesignKit into its own sub-package (#459)
    We'll be using https://github.com/vector-im/compound-ios eventually so this tidies up the Xcode project dependencies in the mean time.

--- Package.swift
+++ Package.swift
@@ -6,30 +6,16 @@ import PackageDescription
 let package = Package(
     name: "Element Swift",
     platforms: [
-        .iOS(.v14),
         .macOS(.v13)
     ],
     products: [
-        .library(name: "DesignKit", targets: ["DesignKit"]),
         .executable(name: "tools", targets: ["Tools"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vector-im/element-design-tokens.git", exact: "0.0.3"),
-        .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.4"),
-        /* Command line tools dependencies */
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
         .package(url: "https://github.com/jpsim/Yams", from: "5.0.1")
     ],
     targets: [
-        .target(name: "DesignKit",
-                dependencies: [
-                    .product(name: "DesignTokens", package: "element-design-tokens"),
-                    .product(name: "Introspect", package: "SwiftUI-Introspect")
-                ],
-                path: "DesignKit"),
-        .testTarget(name: "DesignKitTests",
-                    dependencies: ["DesignKit"],
-                    path: "DesignKitTests"),
         .executableTarget(name: "Tools",
                           dependencies: [
                               .product(name: "ArgumentParser", package: "swift-argument-parser"),
