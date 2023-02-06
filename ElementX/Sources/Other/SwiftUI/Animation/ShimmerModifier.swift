280d1a0144e2b978d9f0840260deb0b1aeed6a01
Doug <douglase@element.io>
2023-02-06 18:26:09 +0000

    Add shimmer to HomeScreen.

---
+++ ElementX/Sources/Other/SwiftUI/Animation/ShimmerModifier.swift
@@ -0,0 +1,91 @@
+//
+// Copyright 2023 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import SwiftUI
+
+struct ShimmerModifier: ViewModifier {
+    @State private var toggle = false
+    
+    private let startStart = UnitPoint(x: -5, y: 0)
+    private let endStart = UnitPoint(x: 0, y: 0)
+    private let startEnd = UnitPoint(x: 1, y: 0)
+    private let endEnd = UnitPoint(x: 5, y: 0)
+    
+    private let stops: [Gradient.Stop] = [
+        .init(color: .black, location: 0),
+        .init(color: .white.opacity(0.7), location: 0.21),
+        .init(color: .white.opacity(0.7), location: 0.25),
+        .init(color: .black, location: 0.39),
+        .init(color: .black, location: 0.61),
+        .init(color: .white.opacity(0.7), location: 0.8),
+        .init(color: .white.opacity(0.7), location: 0.85),
+        .init(color: .black, location: 1)
+    ]
+    
+    private let stops2: [Gradient.Stop] = [
+        .init(color: .black, location: 0),
+        .init(color: .black, location: 0.3),
+        .init(color: .white.opacity(0.7), location: 0.45),
+        .init(color: .white.opacity(0.7), location: 0.55),
+        .init(color: .black, location: 0.7),
+        .init(color: .black, location: 1)
+    ]
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay { overlay }
+    }
+    
+    var overlay: some View {
+        Rectangle()
+            .fill(gradient)
+            .blendMode(.screen)
+            .opacity(0.8)
+            .onAppear {
+                withAnimation(.linear(duration: 1.2).delay(0.5).repeatForever(autoreverses: false)) {
+                    toggle.toggle()
+                }
+            }
+    }
+    
+    var gradient: LinearGradient {
+        LinearGradient(stops: stops,
+                       startPoint: toggle ? startEnd : startStart,
+                       endPoint: toggle ? endEnd : endStart)
+    }
+}
+
+extension View {
+    func shimmer() -> some View {
+        modifier(ShimmerModifier())
+    }
+}
+
+struct ShimmerOverlay_Previews: PreviewProvider {
+    static let viewModel = HomeScreenViewModel(userSession: MockUserSession(clientProxy: MockClientProxy(userID: ""),
+                                                                            mediaProvider: MockMediaProvider()),
+                                               attributedStringBuilder: AttributedStringBuilder())
+    
+    static var previews: some View {
+        VStack {
+            ForEach(0...8, id: \.self) { _ in
+                HomeScreenRoomCell(room: .placeholder(), context: viewModel.context)
+            }
+        }
+        .redacted(reason: .placeholder)
+        .shimmer()
+    }
+}
