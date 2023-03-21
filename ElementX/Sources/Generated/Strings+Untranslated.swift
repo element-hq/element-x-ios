13856652cfc959aaa2afa8841b74e8be53d6638a
Doug <douglase@element.io>
2023-03-21 18:20:23 +0000

    Remove all old Android strings.

--- ElementX/Sources/Generated/Strings+Untranslated.swift
+++ ElementX/Sources/Generated/Strings+Untranslated.swift
@@ -9,54 +9,82 @@ import Foundation
 
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
-extension ElementL10n {
+public enum UntranslatedL10n {
   /// Help us identify issues and improve %@ by sharing anonymous usage data. To understand how people use multiple devices, we’ll generate a random identifier, shared by your devices.
   /// 
   /// You can read all our terms %@.
   public static func analyticsOptInContent(_ p1: Any, _ p2: Any) -> String {
-    return ElementL10n.tr("Untranslated", "analytics_opt_in_content", String(describing: p1), String(describing: p2))
+    return UntranslatedL10n.tr("Untranslated", "analytics_opt_in_content", String(describing: p1), String(describing: p2))
   }
   /// here
-  public static let analyticsOptInContentLink = ElementL10n.tr("Untranslated", "analytics_opt_in_content_link")
+  public static var analyticsOptInContentLink: String { return UntranslatedL10n.tr("Untranslated", "analytics_opt_in_content_link") }
   /// We <b>don't</b> record or profile any account data
-  public static let analyticsOptInListItem1 = ElementL10n.tr("Untranslated", "analytics_opt_in_list_item_1")
+  public static var analyticsOptInListItem1: String { return UntranslatedL10n.tr("Untranslated", "analytics_opt_in_list_item_1") }
   /// We <b>don't</b> share information with third parties
-  public static let analyticsOptInListItem2 = ElementL10n.tr("Untranslated", "analytics_opt_in_list_item_2")
+  public static var analyticsOptInListItem2: String { return UntranslatedL10n.tr("Untranslated", "analytics_opt_in_list_item_2") }
   /// You can turn this off anytime in settings
-  public static let analyticsOptInListItem3 = ElementL10n.tr("Untranslated", "analytics_opt_in_list_item_3")
+  public static var analyticsOptInListItem3: String { return UntranslatedL10n.tr("Untranslated", "analytics_opt_in_list_item_3") }
   /// Help improve %@
   public static func analyticsOptInTitle(_ p1: Any) -> String {
-    return ElementL10n.tr("Untranslated", "analytics_opt_in_title", String(describing: p1))
+    return UntranslatedL10n.tr("Untranslated", "analytics_opt_in_title", String(describing: p1))
   }
   /// Clear all data currently stored on this device?
   /// Sign in again to access your account data and messages.
-  public static let softLogoutClearDataDialogContent = ElementL10n.tr("Untranslated", "soft_logout_clear_data_dialog_content")
+  public static var softLogoutClearDataDialogContent: String { return UntranslatedL10n.tr("Untranslated", "soft_logout_clear_data_dialog_content") }
   /// Clear data
-  public static let softLogoutClearDataDialogTitle = ElementL10n.tr("Untranslated", "soft_logout_clear_data_dialog_title")
+  public static var softLogoutClearDataDialogTitle: String { return UntranslatedL10n.tr("Untranslated", "soft_logout_clear_data_dialog_title") }
   /// Warning: Your personal data (including encryption keys) is still stored on this device.
   /// 
   /// Clear it if you’re finished using this device, or want to sign in to another account.
-  public static let softLogoutClearDataNotice = ElementL10n.tr("Untranslated", "soft_logout_clear_data_notice")
+  public static var softLogoutClearDataNotice: String { return UntranslatedL10n.tr("Untranslated", "soft_logout_clear_data_notice") }
   /// Clear all data
-  public static let softLogoutClearDataSubmit = ElementL10n.tr("Untranslated", "soft_logout_clear_data_submit")
+  public static var softLogoutClearDataSubmit: String { return UntranslatedL10n.tr("Untranslated", "soft_logout_clear_data_submit") }
   /// Clear personal data
-  public static let softLogoutClearDataTitle = ElementL10n.tr("Untranslated", "soft_logout_clear_data_title")
+  public static var softLogoutClearDataTitle: String { return UntranslatedL10n.tr("Untranslated", "soft_logout_clear_data_title") }
   /// Forgot password
-  public static let softLogoutForgotPassword = ElementL10n.tr("Untranslated", "soft_logout_forgot_password")
+  public static var softLogoutForgotPassword: String { return UntranslatedL10n.tr("Untranslated", "soft_logout_forgot_password") }
   /// Sign in to recover encryption keys stored exclusively on this device. You need them to read all of your secure messages on any device.
-  public static let softLogoutSigninE2eWarningNotice = ElementL10n.tr("Untranslated", "soft_logout_signin_e2e_warning_notice")
+  public static var softLogoutSigninE2eWarningNotice: String { return UntranslatedL10n.tr("Untranslated", "soft_logout_signin_e2e_warning_notice") }
   /// Your homeserver (%1$s) admin has signed you out of your account %2$s (%3$s).
   public static func softLogoutSigninNotice(_ p1: UnsafePointer<CChar>, _ p2: UnsafePointer<CChar>, _ p3: UnsafePointer<CChar>) -> String {
-    return ElementL10n.tr("Untranslated", "soft_logout_signin_notice", p1, p2, p3)
+    return UntranslatedL10n.tr("Untranslated", "soft_logout_signin_notice", p1, p2, p3)
   }
   /// Sign in
-  public static let softLogoutSigninTitle = ElementL10n.tr("Untranslated", "soft_logout_signin_title")
+  public static var softLogoutSigninTitle: String { return UntranslatedL10n.tr("Untranslated", "soft_logout_signin_title") }
   /// Untranslated
-  public static let untranslated = ElementL10n.tr("Untranslated", "untranslated")
+  public static var untranslated: String { return UntranslatedL10n.tr("Untranslated", "untranslated") }
   /// Plural format key: "%#@VARIABLE@"
   public static func untranslatedPlural(_ p1: Int) -> String {
-    return ElementL10n.tr("Untranslated", "untranslated_plural", p1)
+    return UntranslatedL10n.tr("Untranslated", "untranslated_plural", p1)
   }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension UntranslatedL10n {
+  static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let languages = Bundle.preferredLanguages
+
+    for language in languages {
+      let translation = trIn(language, table, key, args)
+      if translation != key {
+        return translation
+      }
+    }
+    return key
+  }
+
+  private static func trIn(_ language: String, _ table: String, _ key: String, _ args: CVarArg...) -> String {
+    guard let bundle = Bundle(for: BundleToken.self).lprojBundle(for: language) else {
+      // no translations for the desired language
+      return key
+    }
+    let format = NSLocalizedString(key, tableName: table, bundle: bundle, comment: "")
+    return String(format: format, locale: Locale(identifier: language), arguments: args)
+  }
+}
+
+private final class BundleToken {}
+
--- ElementX/Sources/Screens/Authentication/SoftLogout/View/SoftLogoutScreen.swift
+++ ElementX/Sources/Screens/Authentication/SoftLogout/View/SoftLogoutScreen.swift
@@ -54,20 +54,20 @@ struct SoftLogoutScreen: View {
     /// The title, message and icon at the top of the screen.
     var header: some View {
         VStack(alignment: .leading, spacing: 16) {
-            Text(ElementL10n.softLogoutSigninTitle)
+            Text(UntranslatedL10n.softLogoutSigninTitle)
                 .font(.element.title2Bold)
                 .multilineTextAlignment(.leading)
                 .foregroundColor(.element.primaryContent)
                 .accessibilityIdentifier(A11yIdentifiers.softLogoutScreen.title)
 
-            Text(ElementL10n.softLogoutSigninNotice(context.viewState.credentials.homeserverName, context.viewState.credentials.userDisplayName, context.viewState.credentials.userId))
+            Text(UntranslatedL10n.softLogoutSigninNotice(context.viewState.credentials.homeserverName, context.viewState.credentials.userDisplayName, context.viewState.credentials.userId))
                 .font(.element.body)
                 .multilineTextAlignment(.leading)
                 .foregroundColor(.element.primaryContent)
                 .accessibilityIdentifier(A11yIdentifiers.softLogoutScreen.message)
 
             if context.viewState.showRecoverEncryptionKeysMessage {
-                Text(ElementL10n.softLogoutSigninE2eWarningNotice)
+                Text(UntranslatedL10n.softLogoutSigninE2eWarningNotice)
                     .font(.element.body)
                     .multilineTextAlignment(.leading)
                     .foregroundColor(.element.primaryContent)
@@ -87,7 +87,7 @@ struct SoftLogoutScreen: View {
                 .accessibilityIdentifier(A11yIdentifiers.softLogoutScreen.password)
 
             Button { context.send(viewAction: .forgotPassword) } label: {
-                Text(ElementL10n.softLogoutForgotPassword)
+                Text(UntranslatedL10n.softLogoutForgotPassword)
                     .font(.element.body)
             }
             .frame(maxWidth: .infinity, alignment: .trailing)
@@ -123,13 +123,13 @@ struct SoftLogoutScreen: View {
     /// The text field and submit button where the user enters an email address.
     var clearDataForm: some View {
         VStack(alignment: .leading, spacing: 12) {
-            Text(ElementL10n.softLogoutClearDataTitle)
+            Text(UntranslatedL10n.softLogoutClearDataTitle)
                 .font(.element.title2Bold)
                 .multilineTextAlignment(.leading)
                 .foregroundColor(.element.primaryContent)
                 .accessibilityIdentifier(A11yIdentifiers.softLogoutScreen.clearDataTitle)
 
-            Text(ElementL10n.softLogoutClearDataNotice)
+            Text(UntranslatedL10n.softLogoutClearDataNotice)
                 .font(.element.body)
                 .multilineTextAlignment(.leading)
                 .foregroundColor(.element.primaryContent)
@@ -137,17 +137,17 @@ struct SoftLogoutScreen: View {
                 .padding(.bottom, 12)
 
             Button(action: clearData) {
-                Text(ElementL10n.softLogoutClearDataSubmit)
+                Text(UntranslatedL10n.softLogoutClearDataSubmit)
             }
             .buttonStyle(.elementAction(.xLarge, color: .element.alert))
             .accessibilityIdentifier(A11yIdentifiers.softLogoutScreen.clearData)
-            .alert(ElementL10n.softLogoutClearDataDialogTitle,
+            .alert(UntranslatedL10n.softLogoutClearDataDialogTitle,
                    isPresented: $showingClearDataConfirmation) {
                 Button(L10n.actionSignOut,
                        role: .destructive,
                        action: clearData)
             } message: {
-                Text(ElementL10n.softLogoutClearDataDialogContent)
+                Text(UntranslatedL10n.softLogoutClearDataDialogContent)
             }
         }
     }
