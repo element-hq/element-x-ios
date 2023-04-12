6e2203d66997574ff970b32b741fdfb727d96cee
Mauro Romito <mauro.romito@element.io>
2023-04-12 10:52:46 +0200

    setup project on ci

--- Tools/Sources/SetupProject.swift
+++ Tools/Sources/SetupProject.swift
@@ -4,6 +4,9 @@ import Foundation
 struct SetupProject: ParsableCommand {
     static var configuration = CommandConfiguration(abstract: "A tool to setup the required components to efficiently run and contribute to Element X iOS")
 
+    @Flag(help: "Use this only on ci to avoid installing failing packages")
+    var ci = false
+
     func run() throws {
         try setupGitHooks()
         try brewBundleInstall()
@@ -16,7 +19,10 @@ struct SetupProject: ParsableCommand {
     }
 
     func brewBundleInstall() throws {
-        try Utilities.zsh("brew install xcodegen swiftgen swiftlint swiftformat git-lfs sourcery mint kiliankoe/formulae/swift-outdated localazy/tools/localazy")
+        try Utilities.zsh("brew install xcodegen swiftgen swiftformat git-lfs sourcery mint kiliankoe/formulae/swift-outdated localazy/tools/localazy")
+        if !ci {
+            try Utilities.zsh("brew install swiftlint")
+        }
     }
 
     func mintPackagesInstall() throws {
