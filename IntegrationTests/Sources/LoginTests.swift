4014bfe74a0a38867e3a8bf1c396a5137704d949
Mauro Romito <mauro.romito@element.io>
2023-05-12 20:18:33 +0200

    this integration test is a bit so flaky increased the timing

--- IntegrationTests/Sources/LoginTests.swift
+++ IntegrationTests/Sources/LoginTests.swift
@@ -40,7 +40,7 @@ class LoginTests: XCTestCase {
                 
         let getStartedButton = app.buttons[A11yIdentifiers.onboardingScreen.signIn]
         
-        XCTAssertTrue(getStartedButton.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(getStartedButton.waitForExistence(timeout: 10.0))
         getStartedButton.tap()
 
         let editHomeserverButton = app.buttons[A11yIdentifiers.loginScreen.changeServer]
@@ -48,26 +48,26 @@ class LoginTests: XCTestCase {
         editHomeserverButton.tap()
         
         let homeserverTextField = app.textFields[A11yIdentifiers.changeServerScreen.server]
-        XCTAssertTrue(homeserverTextField.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(homeserverTextField.waitForExistence(timeout: 10.0))
         
         homeserverTextField.clearAndTypeText(app.homeserver)
                 
         let confirmButton = app.buttons[A11yIdentifiers.changeServerScreen.continue]
-        XCTAssertTrue(confirmButton.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(confirmButton.waitForExistence(timeout: 10.0))
         confirmButton.tap()
         
         let usernameTextField = app.textFields[A11yIdentifiers.loginScreen.emailUsername]
-        XCTAssertTrue(usernameTextField.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(usernameTextField.waitForExistence(timeout: 10.0))
         
         usernameTextField.clearAndTypeText(app.username)
         
         let passwordTextField = app.secureTextFields[A11yIdentifiers.loginScreen.password]
-        XCTAssertTrue(passwordTextField.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(passwordTextField.waitForExistence(timeout: 10.0))
         
         passwordTextField.clearAndTypeText(app.password)
         
         let nextButton = app.buttons[A11yIdentifiers.loginScreen.continue]
-        XCTAssertTrue(nextButton.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(nextButton.waitForExistence(timeout: 10.0))
         XCTAssertTrue(nextButton.isEnabled)
         
         nextButton.tap()
@@ -79,38 +79,38 @@ class LoginTests: XCTestCase {
         
         // Handle save password sheet
         let savePasswordButton = app.buttons["Save Password"]
-        if savePasswordButton.waitForExistence(timeout: 1.0) {
+        if savePasswordButton.waitForExistence(timeout: 10.0) {
             savePasswordButton.tap()
         }
         
         // Handle analytics prompt screen
         if app.staticTexts[A11yIdentifiers.analyticsPromptScreen.title].waitForExistence(timeout: 1.0) {
             let enableButton = app.buttons[A11yIdentifiers.analyticsPromptScreen.enable]
-            XCTAssertTrue(enableButton.waitForExistence(timeout: 1.0))
+            XCTAssertTrue(enableButton.waitForExistence(timeout: 10.0))
             enableButton.tap()
         }
         
         // Handle the notifications permission alert https://stackoverflow.com/a/58171074/730924
         let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
         let alertAllowButton = springboard.buttons.element(boundBy: 1)
-        if alertAllowButton.waitForExistence(timeout: 1.0) {
+        if alertAllowButton.waitForExistence(timeout: 10.0) {
             alertAllowButton.tap()
         }
         
         let profileButton = app.buttons[A11yIdentifiers.homeScreen.userAvatar]
-        XCTAssertTrue(profileButton.waitForExistence(timeout: 2.0))
+        XCTAssertTrue(profileButton.waitForExistence(timeout: 10.0))
 
         // `Failed to scroll to visible (by AX action) Button` https://stackoverflow.com/a/33534187/730924
         profileButton.forceTap()
         
         let menuLogoutButton = app.buttons["Sign out"]
-        XCTAssertTrue(menuLogoutButton.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(menuLogoutButton.waitForExistence(timeout: 10.0))
         menuLogoutButton.tap()
         
         let alertLogoutButton = app.buttons["Sign out"]
-        XCTAssertTrue(alertLogoutButton.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(alertLogoutButton.waitForExistence(timeout: 10.0))
         alertLogoutButton.tap()
         
-        XCTAssertTrue(getStartedButton.waitForExistence(timeout: 1.0))
+        XCTAssertTrue(getStartedButton.waitForExistence(timeout: 10.0))
     }
 }
