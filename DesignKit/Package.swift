59b0a1e1bea191432381b695c30a1b2f7a872a2b
Stefan Ceriu <stefanc@matrix.org>
2023-05-15 12:27:30 +0300

    Bump compound-ios to its latest revision

--- DesignKit/Package.swift
+++ DesignKit/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "DesignKit", targets: ["DesignKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vector-im/compound-ios.git", revision: "b4f34edce9003ba1fde31500312f317b728f4ee3"),
+        .package(url: "https://github.com/vector-im/compound-ios.git", revision: "a6aec9a77bf008c86a296ba17d60005b5a8bfae4"),
         .package(url: "https://github.com/vector-im/element-design-tokens.git", exact: "0.0.3"),
         .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.4")
     ],
