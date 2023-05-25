c3eb64bde59c394ae50c789b9acf29df5daad6b5
Mauro <34335419+Velin92@users.noreply.github.com>
2023-05-25 13:15:12 +0200

    Fix for flaky corner radius path generation (#958)
    * fixed the flaky corner radius path generation
    
    * format

--- ElementX/Sources/Other/SwiftUI/Views/RoundedCornerShape.swift
+++ ElementX/Sources/Other/SwiftUI/Views/RoundedCornerShape.swift
@@ -26,10 +26,41 @@ struct RoundedCornerShape: Shape {
     }
 
     func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect,
-                                byRoundingCorners: corners,
-                                cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
+        var path = Path()
+
+        let width = rect.size.width
+        let height = rect.size.height
+
+        var topLeft: CGFloat = corners.contains(.topLeft) ? radius : 0.0
+        var topRight: CGFloat = corners.contains(.topRight) ? radius : 0.0
+        var bottomLeft: CGFloat = corners.contains(.bottomLeft) ? radius : 0.0
+        var bottomRight: CGFloat = corners.contains(.bottomRight) ? radius : 0.0
+
+        // Make sure we do not exceed the size of the rectangle
+        topRight = min(min(topRight, height / 2), width / 2)
+        topLeft = min(min(topLeft, height / 2), width / 2)
+        bottomLeft = min(min(bottomLeft, height / 2), width / 2)
+        bottomRight = min(min(bottomRight, height / 2), width / 2)
+
+        path.move(to: CGPoint(x: width / 2.0, y: 0))
+        path.addLine(to: CGPoint(x: width - topRight, y: 0))
+        path.addArc(center: CGPoint(x: width - topRight, y: topRight), radius: topRight,
+                    startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
+
+        path.addLine(to: CGPoint(x: width, y: height - bottomRight))
+        path.addArc(center: CGPoint(x: width - bottomRight, y: height - bottomRight), radius: bottomRight,
+                    startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
+
+        path.addLine(to: CGPoint(x: bottomLeft, y: height))
+        path.addArc(center: CGPoint(x: bottomLeft, y: height - bottomLeft), radius: bottomLeft,
+                    startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
+
+        path.addLine(to: CGPoint(x: 0, y: topLeft))
+        path.addArc(center: CGPoint(x: topLeft, y: topLeft), radius: topLeft,
+                    startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+        path.closeSubpath()
+
+        return path
     }
 }
 
