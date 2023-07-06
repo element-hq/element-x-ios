dedd93c46b0a48e0cc33fee1cb36addd83cb5f7b
Alfonso Grillo <alfogrillo@gmail.com>
2023-07-06 11:37:43 +0200

    Filter account owner in UserDiscoveryService

--- ElementX/Sources/Services/Users/UserDiscoveryService.swift
+++ ElementX/Sources/Services/Users/UserDiscoveryService.swift
@@ -22,35 +22,35 @@ final class UserDiscoveryService: UserDiscoveryServiceProtocol {
     init(clientProxy: ClientProxyProtocol) {
         self.clientProxy = clientProxy
     }
-    
+
     func fetchSuggestions() async -> Result<[UserProfileProxy], UserDiscoveryErrorType> {
-        .success([.mockAlice, .mockBob, .mockCharlie])
+        .success(filterAccountOwner([.mockAlice, .mockBob, .mockCharlie]))
     }
-    
+
     func searchProfiles(with searchQuery: String) async -> Result<[UserProfileProxy], UserDiscoveryErrorType> {
         async let queriedProfile = profileIfPossible(with: searchQuery)
-        
+
         do {
             async let searchedUsers = clientProxy.searchUsers(searchTerm: searchQuery, limit: 10).get()
             let users = try await merge(queriedProfile: queriedProfile, searchResults: searchedUsers)
-            return .success(users)
+            return .success(filterAccountOwner(users))
         } catch {
             // we want to show the profile (if any) even if the search fails
             if let queriedProfile = await queriedProfile {
-                return .success([queriedProfile])
+                return .success(filterAccountOwner([queriedProfile]))
             } else {
                 return .failure(.failedSearchingUsers)
             }
         }
     }
-    
+
     private func merge(queriedProfile: UserProfileProxy?, searchResults: SearchUsersResultsProxy) -> [UserProfileProxy] {
         let searchResults = searchResults.results
         
         guard let queriedProfile else {
             return searchResults
         }
-        
+
         let filteredSearchResult = searchResults.filter {
             $0.userID != queriedProfile.userID
         }
@@ -67,6 +67,11 @@ final class UserDiscoveryService: UserDiscoveryServiceProtocol {
         
         // fallback to a "local profile" if the profile api fails
         return getProfileResult ?? .init(userID: searchQuery)
+    }
+
+    private func filterAccountOwner(_ profiles: [UserProfileProxy]) -> [UserProfileProxy] {
+        let accountOwnerID = clientProxy.userID
+        return profiles.filter { $0.userID != accountOwnerID }
     }
 }
 
