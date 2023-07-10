633a018077e8d36ffd748e2fc36163b43694064f
Mauro Romito <mauro.romito@element.io>
2023-07-10 11:20:44 +0200

    notification refactor

--- NSE/Sources/Other/NSEUserSession.swift
+++ NSE/Sources/Other/NSEUserSession.swift
@@ -18,56 +18,39 @@ import Foundation
 import MatrixRustSDK
 
 final class NSEUserSession {
-    private let client: ClientProtocol
+    private let baseClient: Client
+    private let notificationClient: NotificationClient
     private let userID: String
-    private var encryptionSyncService: EncryptionSync?
-    private(set) lazy var mediaProvider: MediaProviderProtocol = MediaProvider(mediaLoader: MediaLoader(client: client),
+    private(set) lazy var mediaProvider: MediaProviderProtocol = MediaProvider(mediaLoader: MediaLoader(client: baseClient),
                                                                                imageCache: .onlyOnDisk,
                                                                                backgroundTaskService: nil)
 
-    var isSyncing: Bool {
-        encryptionSyncService != nil
-    }
-
-    init(credentials: KeychainCredentials) throws {
+    init(credentials: KeychainCredentials, isEncryptionSyncEnabled: Bool) throws {
         userID = credentials.userID
-        let builder = ClientBuilder()
+        baseClient = try ClientBuilder()
             .basePath(path: URL.sessionsBaseDirectory.path)
             .username(username: credentials.userID)
+            .build()
 
-        client = try builder.build()
-        try client.restoreSession(session: credentials.restorationToken.session)
-    }
+        try baseClient.restoreSession(session: credentials.restorationToken.session)
 
-    func startEncryptionSync() throws {
-        let listener = EncryptionSyncListenerProxy { [weak self] reason in
-            MXLog.info("NSE: Encryption sync terminated for user: \(self?.userID ?? "unknown") with reason: \(reason)")
-            self?.encryptionSyncService = nil
-        }
-        encryptionSyncService = try client.notificationEncryptionSync(id: "NSE", listener: listener, numIters: 2)
-        MXLog.info("NSE: Encryption sync started for user: \(userID)")
+        notificationClient = baseClient
+            .notificationClient()
+            .retryDecryption(withCrossProcessLock: isEncryptionSyncEnabled)
+            .finish()
     }
 
     func notificationItemProxy(roomID: String, eventID: String) async -> NotificationItemProxyProtocol? {
         await Task.dispatch(on: .global()) {
             do {
-                guard let notification = try self.client.getNotificationItem(roomId: roomID, eventId: eventID, filterByPushRules: false) else {
+                guard let notification = try self.notificationClient.getNotification(roomId: roomID, eventId: eventID) else {
                     return nil
                 }
-                return NotificationItemProxy(notificationItem: notification, receiverID: self.userID)
+                return NotificationItemProxy(notificationItem: notification, receiverID: self.userID, roomID: roomID)
             } catch {
                 MXLog.error("NSE: Could not get notification's content creating an empty notification instead, error: \(error)")
                 return EmptyNotificationItemProxy(eventID: eventID, roomID: roomID, receiverID: self.userID)
             }
         }
-    }
-
-    func stopEncryptionSync() {
-        encryptionSyncService?.stop()
-    }
-
-    deinit {
-        MXLog.info("NSE: NSEUserSession deinit called for user: \(userID)")
-        stopEncryptionSync()
     }
 }
