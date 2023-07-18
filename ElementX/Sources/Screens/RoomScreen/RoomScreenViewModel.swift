cf397108910179d0e9e33c4a7146a54b7ac02e55
Alfonso Grillo <alfogrillo@gmail.com>
2023-07-18 17:54:42 +0200

    Remove ”copy” action for location events (#1355)
    * Hide ”copy” action for location events
    
    * Cleanup

--- ElementX/Sources/Screens/RoomScreen/RoomScreenViewModel.swift
+++ ElementX/Sources/Screens/RoomScreen/RoomScreenViewModel.swift
@@ -411,7 +411,7 @@ class RoomScreenViewModel: RoomScreenViewModelType, RoomScreenViewModelProtocol 
             actions.append(.edit)
         }
 
-        if item.isMessage {
+        if item.isMessage, !item.isLocation {
             actions.append(.copy)
         }
         
