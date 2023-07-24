c9f3f4eddd6bb924645b865c5f9b48fcaf7a55dc
Mauro Romito <mauro.romito@element.io>
2023-07-24 22:40:22 +0200

    doc

--- ElementX/Sources/Screens/RoomScreen/View/Supplementary/TimelineItemStatusView.swift
+++ ElementX/Sources/Screens/RoomScreen/View/Supplementary/TimelineItemStatusView.swift
@@ -21,6 +21,8 @@ struct TimelineItemStatusView: View {
     @Environment(\.timelineStyle) private var style
     @Environment(\.readReceiptsEnabled) private var readReceiptsEnabled
     @EnvironmentObject private var context: RoomScreenViewModel.Context
+    
+    // Required since the timelineViewState is a reference and its changes are not observed by the context
     @ObservedObject var timelineViewState: TimelineViewState
 
     private var isLastOutgoingMessage: Bool {
