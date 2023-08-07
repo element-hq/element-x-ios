22c0e38c5d4214ed8d35b5f10440ba5dc1b7a9fc
Doug <6060466+pixlwave@users.noreply.github.com>
2023-08-07 14:52:54 +0100

    Upload to sentry before releasing/tagging. (#1457)
    This is to prevent tags being created for CI runs that have failed.

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,6 +4,10 @@ source ci_common.sh
 
 setup_xcode_cloud_environment
 
+# Upload dsyms no matter the workflow
+# Perform this step before releasing to github in case it fails.
+bundle exec fastlane upload_dsyms_to_sentry dsym_path:"$CI_ARCHIVE_PATH/dSYMs"
+
 if [ "$CI_WORKFLOW" = "Release" ]; then
     install_xcode_cloud_python_dependencies
 
@@ -12,6 +16,3 @@ if [ "$CI_WORKFLOW" = "Release" ]; then
 elif [ "$CI_WORKFLOW" = "Nightly" ]; then
     bundle exec fastlane tag_nightly build_number:"$CI_BUILD_NUMBER"
 fi
-
-# Upload dsyms no matter the workflow
-bundle exec fastlane upload_dsyms_to_sentry dsym_path:"$CI_ARCHIVE_PATH/dSYMs"
