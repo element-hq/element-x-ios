4b1083bf5eaa6a9f31233fbc289ada7fd28a3c87
Stefan Ceriu <stefanc@matrix.org>
2023-08-22 15:59:17 +0300

    Increase room summary provider diff processing queue priority

--- ElementX/Sources/Services/Room/RoomSummary/RoomSummaryProvider.swift
+++ ElementX/Sources/Services/Room/RoomSummary/RoomSummaryProvider.swift
@@ -60,7 +60,7 @@ class RoomSummaryProvider: RoomSummaryProviderProtocol {
          appSettings: AppSettings,
          backgroundTaskService: BackgroundTaskServiceProtocol) {
         self.roomListService = roomListService
-        serialDispatchQueue = DispatchQueue(label: "io.element.elementx.roomsummaryprovider", qos: .utility)
+        serialDispatchQueue = DispatchQueue(label: "io.element.elementx.roomsummaryprovider", qos: .default)
         self.eventStringBuilder = eventStringBuilder
         self.name = name
         self.appSettings = appSettings
