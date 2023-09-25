1f3898c69d85b6b1e690eb8be11b009336762a4b
Mauro <34335419+Velin92@users.noreply.github.com>
2023-09-25 18:17:36 +0200

    remove `reply in thread` in the swipe action (#1795)
    * remove `reply in thread` in the swipe action
    
    * changelog
    
    * removed v stack

--- ElementX/Sources/Screens/RoomScreen/View/Style/SwipeToReplyView.swift
+++ ElementX/Sources/Screens/RoomScreen/View/Style/SwipeToReplyView.swift
@@ -20,17 +20,9 @@ struct SwipeToReplyView: View {
     let timelineItem: RoomTimelineItemProtocol
     
     var body: some View {
-        VStack(spacing: 4) {
-            Image(systemName: "arrowshape.turn.up.left")
-                .foregroundColor(.compound.iconPrimary)
-            if let messageTimelineItem = timelineItem as? EventBasedMessageTimelineItemProtocol,
-               messageTimelineItem.isThreaded {
-                Text(L10n.actionReplyInThread)
-                    .font(.compound.bodyXS)
-                    .foregroundColor(.compound.textPrimary)
-            }
-        }
-        .accessibilityHidden(true)
+        Image(systemName: "arrowshape.turn.up.left")
+            .foregroundColor(.compound.iconPrimary)
+            .accessibilityHidden(true)
     }
 }
 
@@ -41,16 +33,8 @@ struct SwipeToReplyView_Previews: PreviewProvider, TestablePreview {
                                                    isEditable: true,
                                                    isThreaded: false, sender: .init(id: ""),
                                                    content: .init(body: ""))
-    static let threadedTimelineItem = TextRoomTimelineItem(id: .init(timelineID: ""),
-                                                           timestamp: "",
-                                                           isOutgoing: true,
-                                                           isEditable: true,
-                                                           isThreaded: true,
-                                                           sender: .init(id: ""),
-                                                           content: .init(body: ""))
     
     static var previews: some View {
         SwipeToReplyView(timelineItem: timelineItem)
-        SwipeToReplyView(timelineItem: threadedTimelineItem)
     }
 }
