8d473df2e9b271e4514c2df45bfb602b79fa6ea2
Mauro Romito <mauro.romito@element.io>
2023-10-04 19:43:44 +0200

    created the list but I need to find a way to overlay it

---
+++ ElementX/Sources/Screens/ComposerToolbar/View/CompletionSuggestionView.swift
@@ -0,0 +1,131 @@
+//
+// Copyright 2021 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import SwiftUI
+
+struct CompletionSuggestionView: View {
+    let imageProvider: ImageProviderProtocol?
+    let items: [SuggestionItem]
+    
+    private enum Constants {
+        static let topPadding: CGFloat = 8.0
+        static let listItemPadding: CGFloat = 4.0
+        static let lineSpacing: CGFloat = 10.0
+        static let maxHeight: CGFloat = 300.0
+        static let maxVisibleRows = 4
+
+        /*
+         As of iOS 16.0, SwiftUI's List uses `UICollectionView` instead
+         of `UITableView` internally, this value is an adjustment to apply
+         to the list items in order to be as close as possible as the
+         `UITableView` display.
+         */
+        @available(iOS 16.0, *)
+        static let collectionViewPaddingCorrection: CGFloat = -5.0
+    }
+
+    // MARK: Public
+    
+    var showBackgroundShadow = true
+    
+    var body: some View {
+        if items.isEmpty {
+            EmptyView()
+        } else {
+            if showBackgroundShadow {
+                BackgroundView {
+                    list()
+                }
+            } else {
+                list()
+            }
+        }
+    }
+
+    private func list() -> some View {
+        List(items) { item in
+            Button { } label: {
+                switch item {
+                case .user(let mention):
+                    MentionSuggestionItemView(imageProvider: imageProvider, item: mention)
+                }
+            }
+            .modifier(ListItemPaddingModifier(isFirst: items.first?.id == item.id))
+        }
+        .listStyle(PlainListStyle())
+    }
+
+    private struct ListItemPaddingModifier: ViewModifier {
+        private let isFirst: Bool
+
+        init(isFirst: Bool) {
+            self.isFirst = isFirst
+        }
+
+        func body(content: Content) -> some View {
+            var topPadding: CGFloat = isFirst ? Constants.listItemPadding + Constants.topPadding : Constants.listItemPadding
+            var bottomPadding: CGFloat = Constants.listItemPadding
+            if #available(iOS 16.0, *) {
+                topPadding += Constants.collectionViewPaddingCorrection
+                bottomPadding += Constants.collectionViewPaddingCorrection
+            }
+
+            return content
+                .padding(.top, topPadding)
+                .padding(.bottom, bottomPadding)
+        }
+    }
+}
+
+private struct BackgroundView<Content: View>: View {
+    var content: () -> Content
+    
+    private let shadowRadius: CGFloat = 20.0
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        content()
+            .background(Color.compound.bgSubtlePrimary)
+            .clipShape(RoundedCornerShape(radius: shadowRadius, corners: [.topLeft, .topRight]))
+            .shadow(color: .black.opacity(0.20), radius: 20.0, x: 0.0, y: 3.0)
+            .mask(Rectangle().padding(.init(top: -(shadowRadius * 2), leading: 0.0, bottom: 0.0, trailing: 0.0)))
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+// MARK: - Previews
+
+struct CompletionSuggestion_Previews: PreviewProvider, TestablePreview {
+    static let testValue = MentionSuggestionItem(id: "Test", displayName: "Test", avatarURL: nil)
+    
+    static var previews: some View {
+        CompletionSuggestionView(imageProvider: MockMediaProvider(), items: .init(repeating: .user(item: testValue), count: 5))
+    }
+}
+
+enum SuggestionItem: Identifiable {
+    case user(item: MentionSuggestionItem)
+    
+    var id: String {
+        switch self {
+        case .user(let user):
+            return user.id
+        }
+    }
+}
---
+++ ElementX/Sources/Screens/ComposerToolbar/View/MentionSuggestionItemView.swift
@@ -0,0 +1,58 @@
+//
+// Copyright 2023 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import SwiftUI
+
+struct MentionSuggestionItemView: View {
+    let imageProvider: ImageProviderProtocol?
+    let item: MentionSuggestionItem
+    
+    var body: some View {
+        HStack(alignment: .center) {
+            LoadableAvatarImage(url: item.avatarURL,
+                                name: item.displayName,
+                                contentID: item.id,
+                                avatarSize: .custom(42),
+                                imageProvider: imageProvider)
+            VStack(alignment: .leading) {
+                Text(item.displayName ?? item.id)
+                    .font(.compound.bodyLG)
+                    .foregroundColor(.compound.textPrimary)
+                    .lineLimit(1)
+                if item.displayName != nil {
+                    Text(item.id)
+                        .font(.compound.bodySM)
+                        .foregroundColor(.compound.textSecondary)
+                }
+            }
+        }
+    }
+}
+
+struct MentionSuggestionItemView_Previews: PreviewProvider, TestablePreview {
+    static let mockMediaProvider = MockMediaProvider()
+    
+    static var previews: some View {
+        MentionSuggestionItemView(imageProvider: mockMediaProvider, item: .init(id: "test", displayName: "Test", avatarURL: URL.documentsDirectory))
+        MentionSuggestionItemView(imageProvider: mockMediaProvider, item: .init(id: "test2", displayName: nil, avatarURL: nil))
+    }
+}
+
+struct MentionSuggestionItem: Identifiable {
+    let id: String
+    let displayName: String?
+    let avatarURL: URL?
+}
