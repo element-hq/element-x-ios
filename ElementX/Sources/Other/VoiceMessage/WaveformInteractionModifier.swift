186f9f1d67ff62b3758ad5970a68a5acd92dd266
Alfonso Grillo <alfogrillo@gmail.com>
2023-10-27 08:37:10 +0200

    Refine cursor size

--- ElementX/Sources/Other/VoiceMessage/WaveformInteractionModifier.swift
+++ ElementX/Sources/Other/VoiceMessage/WaveformInteractionModifier.swift
@@ -29,7 +29,8 @@ private struct WaveformInteractionModifier: ViewModifier {
     let onSeek: (Double) -> Void
 
     @ScaledMetric private var cursorVisibleWidth = 2.0
-    private let cursorInteractiveWidth: CGFloat = 50
+    @ScaledMetric private var cursorVisibleHeight = 24.0
+    private let cursorInteractiveSize: CGFloat = 50
     private let feedbackGenerator = UISelectionFeedbackGenerator()
 
     func body(content: Content) -> some View {
@@ -43,9 +44,10 @@ private struct WaveformInteractionModifier: ViewModifier {
                     })
                 .progressCursor(progress: progress) {
                     WaveformCursorView(color: .compound.iconAccentTertiary)
-                        .frame(width: cursorVisibleWidth)
+                        .frame(width: cursorVisibleWidth, height: cursorVisibleHeight)
                         .opacity(showCursor ? 1 : 0)
-                        .frame(width: cursorInteractiveWidth)
+                        .frame(width: cursorInteractiveSize)
+                        .frame(maxHeight: cursorInteractiveSize)
                         .contentShape(Rectangle())
                         .gesture(DragGesture(coordinateSpace: .named(Self.namespaceName))
                             .updating(isDragging) { dragGesture, isDragging, _ in
@@ -55,7 +57,7 @@ private struct WaveformInteractionModifier: ViewModifier {
                                 onSeek(max(0, min(progress, 1.0)))
                             }
                         )
-                        .offset(x: -cursorInteractiveWidth / 2, y: 0)
+                        .offset(x: -cursorInteractiveSize / 2, y: 0)
                 }
         }
         .coordinateSpace(name: Self.namespaceName)
