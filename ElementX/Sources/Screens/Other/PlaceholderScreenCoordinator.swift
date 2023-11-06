2cd06a614c37c7658b3dcc16e916e7575b54bf57
Doug <douglase@element.io>
2023-11-06 10:46:18 +0000

    Comments.

--- ElementX/Sources/Screens/Other/PlaceholderScreenCoordinator.swift
+++ ElementX/Sources/Screens/Other/PlaceholderScreenCoordinator.swift
@@ -42,8 +42,8 @@ struct PlaceholderScreen: View {
             }
             .background()
             .environment(\.backgroundStyle, AnyShapeStyle(Color.compound.bgCanvasDefault))
-            .ignoresSafeArea(edges: .top)
-            .ignoresSafeArea(.keyboard)
+            .ignoresSafeArea(edges: .top) // Remain vertically centred even if there's a navigation bar.
+            .ignoresSafeArea(.keyboard) // Specifically for the lock screen, but make sense everywhere.
     }
 }
 
