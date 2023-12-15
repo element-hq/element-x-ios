711ad2ed548f37be8db8a4c5468c594d80363615
Mauro Romito <mauro.romito@element.io>
2023-12-15 18:11:47 +0100

    restored code that was needed

--- ElementX/Sources/Screens/RoomScreen/View/Timeline/PollRoomTimelineView.swift
+++ ElementX/Sources/Screens/RoomScreen/View/Timeline/PollRoomTimelineView.swift
@@ -157,50 +157,62 @@ struct PollRoomTimelineView_Previews: PreviewProvider, TestablePreview {
 
     static var previews: some View {
         PollRoomTimelineView(timelineItem: .mock(poll: .disclosed(), isOutgoing: false))
+            .environment(\.timelineStyle, .bubbles)
             .environmentObject(viewModel.context)
             .previewDisplayName("Disclosed, Bubble")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .undisclosed(), isOutgoing: false))
+            .environment(\.timelineStyle, .bubbles)
             .environmentObject(viewModel.context)
             .previewDisplayName("Undisclosed, Bubble")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .endedDisclosed))
+            .environment(\.timelineStyle, .bubbles)
             .environmentObject(viewModel.context)
             .previewDisplayName("Ended, Disclosed, Bubble")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .endedUndisclosed))
+            .environment(\.timelineStyle, .bubbles)
             .environmentObject(viewModel.context)
             .previewDisplayName("Ended, Undisclosed, Bubble")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .disclosed(createdByAccountOwner: true)))
+            .environment(\.timelineStyle, .bubbles)
             .environmentObject(viewModel.context)
             .previewDisplayName("Creator, disclosed, Bubble")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .disclosed(), isOutgoing: false))
+            .environment(\.timelineStyle, .plain)
             .environmentObject(viewModel.context)
             .previewDisplayName("Disclosed, Plain")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .undisclosed(), isOutgoing: false))
+            .environment(\.timelineStyle, .plain)
             .environmentObject(viewModel.context)
             .previewDisplayName("Undisclosed, Plain")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .endedDisclosed))
+            .environment(\.timelineStyle, .plain)
             .environmentObject(viewModel.context)
             .previewDisplayName("Ended, Disclosed, Plain")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .endedUndisclosed))
+            .environment(\.timelineStyle, .plain)
             .environmentObject(viewModel.context)
             .previewDisplayName("Ended, Undisclosed, Plain")
 
         PollRoomTimelineView(timelineItem: .mock(poll: .disclosed(createdByAccountOwner: true)))
+            .environment(\.timelineStyle, .plain)
             .environmentObject(viewModel.context)
             .previewDisplayName("Creator, disclosed, Plain")
         
         PollRoomTimelineView(timelineItem: .mock(poll: .emptyDisclosed, isEditable: true))
+            .environment(\.timelineStyle, .bubbles)
             .environmentObject(viewModel.context)
             .previewDisplayName("Creator, no votes, Bubble")
         
         PollRoomTimelineView(timelineItem: .mock(poll: .emptyDisclosed, isEditable: true))
+            .environment(\.timelineStyle, .plain)
             .environmentObject(viewModel.context)
             .previewDisplayName("Creator, no votes, Plain")
     }
