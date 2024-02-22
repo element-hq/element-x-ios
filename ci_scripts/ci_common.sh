14827333384b98d24ad291d9409ef910b8dada9f
Doug <douglase@element.io>
2024-02-22 10:23:19 +0000

    Try xcresultparser for coverage.

--- ci_scripts/ci_common.sh
+++ ci_scripts/ci_common.sh
@@ -41,7 +41,7 @@ setup_github_actions_environment() {
     unset HOMEBREW_NO_INSTALL_FROM_API
     export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
     
-    brew update && brew install xcodegen swiftformat git-lfs imagemagick
+    brew update && brew install xcodegen swiftformat git-lfs imagemagick a7ex/homebrew-formulae/xcresultparser
     
     # brew "swiftlint" # Fails on the CI: `Target /usr/local/bin/swiftlint Target /usr/local/bin/swiftlint already exists`. Installed through https://github.com/actions/virtual-environments/blob/main/images/macos/macos-12-Readme.md#linters
 
