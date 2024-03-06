9bba826168fa52e8dc335e8aa02cee1f27ae51b9
Doug <douglase@element.io>
2024-03-06 13:04:20 +0000

    Add RoomChangeRolesScreen.
    Not part of a flow yet, just the screen.

---
+++ ElementX/Sources/Screens/RoomChangeRolesScreen/RoomChangeRolesScreenModels.swift
@@ -0,0 +1,100 @@
+//
+// Copyright 2022 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Collections
+import Foundation
+
+enum RoomChangeRolesScreenViewModelAction {
+    case done
+}
+
+struct RoomChangeRolesScreenViewState: BindableState {
+    /// The screen's current mode (which role we are promoting/demoting users to/from.
+    let mode: RoomMemberDetails.Role
+    /// All of the room's members.
+    var members: [RoomMemberDetails]
+    var bindings: RoomChangeRolesScreenViewStateBindings
+    
+    /// The members selected for promotion to the current role.
+    var membersToPromote: Set<RoomMemberDetails> = []
+    /// The member selected for demotion back to a regular user.
+    var membersToDemote: Set<RoomMemberDetails> = []
+    
+    /// The last member added to the carousel at the top of the screen.
+    var lastPromotedMember: RoomMemberDetails?
+    
+    /// The screen's title.
+    var title: String {
+        switch mode {
+        case .administrator:
+            L10n.screenRoomChangeRoleAdministratorsTitle
+        case .moderator:
+            L10n.screenRoomChangeRoleModeratorsTitle
+        case .user:
+            ""
+        }
+    }
+    
+    /// The visible members in the screen (after searching).
+    var visibleMembers: [RoomMemberDetails] {
+        guard !bindings.searchQuery.isEmpty else { return members }
+        
+        return members.filter {
+            $0.name?.localizedStandardContains(bindings.searchQuery) == true || $0.id.localizedStandardContains(bindings.searchQuery)
+        }
+    }
+    
+    /// All of the members who will gain/keep this screen's role after saving any changes.
+    var membersWithRole: [RoomMemberDetails] {
+        members.filter(isMemberSelected)
+    }
+    
+    /// Whether or not any changes have been made to the members.
+    var hasChanges: Bool {
+        !membersToPromote.isEmpty || !membersToDemote.isEmpty
+    }
+    
+    /// Whether or not the user is searching.
+    var isSearching: Bool {
+        !bindings.searchQuery.isEmpty
+    }
+    
+    /// Whether or not a specific member has this screen's role.
+    func isMemberSelected(_ member: RoomMemberDetails) -> Bool {
+        guard !membersToDemote.contains(member) else { return false }
+        return member.role == mode || membersToPromote.contains(member)
+    }
+}
+
+struct RoomChangeRolesScreenViewStateBindings {
+    var searchQuery = ""
+    /// Information about the currently displayed alert.
+    var alertInfo: AlertInfo<RoomChangePermissionsScreenAlertType>?
+}
+
+enum RoomChangeRolesScreenAlertType {
+    /// The generic error message.
+    case generic
+}
+
+enum RoomChangeRolesScreenViewAction {
+    /// Promote/Demote the specified member, toggling their role between user and this screen's role.
+    case toggleMember(RoomMemberDetails)
+    /// Demote the specified member to a regular user.
+    case demoteMember(RoomMemberDetails)
+    /// Save all the changes that the user has made.
+    case save
+}
---
+++ UnitTests/Sources/RoomChangeRolesScreenViewModelTests.swift
@@ -0,0 +1,176 @@
+//
+// Copyright 2022 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import XCTest
+
+@testable import ElementX
+
+@MainActor
+class RoomChangeRolesScreenViewModelTests: XCTestCase {
+    var viewModel: RoomChangeRolesScreenViewModelProtocol!
+    var roomProxy: RoomProxyMock!
+    
+    var context: RoomChangeRolesScreenViewModelType.Context {
+        viewModel.context
+    }
+
+    func testInitialStateAdministrators() {
+        setupRoomProxy()
+        viewModel = RoomChangeRolesScreenViewModel(mode: .administrator, roomProxy: roomProxy, userIndicatorController: UserIndicatorControllerMock())
+        XCTAssertEqual(context.viewState.membersToPromote, [])
+        XCTAssertEqual(context.viewState.membersToDemote, [])
+        XCTAssertEqual(context.viewState.members, context.viewState.visibleMembers)
+        XCTAssertEqual(context.viewState.membersWithRole.count, 1)
+        XCTAssertEqual(context.viewState.membersWithRole.first?.id, RoomMemberProxyMock.mockAdmin.userID)
+        XCTAssertFalse(context.viewState.hasChanges)
+        XCTAssertFalse(context.viewState.isSearching)
+    }
+
+    func testInitialStateModerators() {
+        setupRoomProxy()
+        viewModel = RoomChangeRolesScreenViewModel(mode: .moderator, roomProxy: roomProxy, userIndicatorController: UserIndicatorControllerMock())
+        XCTAssertEqual(context.viewState.membersToPromote, [])
+        XCTAssertEqual(context.viewState.membersToDemote, [])
+        XCTAssertEqual(context.viewState.members, context.viewState.visibleMembers)
+        XCTAssertEqual(context.viewState.membersWithRole.count, 1)
+        XCTAssertEqual(context.viewState.membersWithRole.first?.id, RoomMemberProxyMock.mockModerator.userID)
+        XCTAssertFalse(context.viewState.hasChanges)
+        XCTAssertFalse(context.viewState.isSearching)
+    }
+    
+    func testToggleUserOn() {
+        testInitialStateModerators()
+        guard let firstUser = context.viewState.members.filter({ !context.viewState.isMemberSelected($0) }).first else {
+            XCTFail("There should be a regular user available to promote.")
+            return
+        }
+        
+        context.send(viewAction: .toggleMember(firstUser))
+        
+        XCTAssertEqual(context.viewState.membersToPromote, [firstUser])
+        XCTAssertEqual(context.viewState.membersToDemote, [])
+        XCTAssertEqual(context.viewState.membersWithRole.count, 2)
+        XCTAssertTrue(context.viewState.membersWithRole.contains(firstUser))
+        XCTAssertTrue(context.viewState.hasChanges)
+    }
+    
+    func testToggleUserOff() {
+        testToggleUserOn()
+        guard let firstUser = context.viewState.membersToPromote.first else {
+            XCTFail("There should be a promoted member before we begin.")
+            return
+        }
+        
+        context.send(viewAction: .toggleMember(firstUser))
+        
+        XCTAssertEqual(context.viewState.membersToPromote, [])
+        XCTAssertEqual(context.viewState.membersToDemote, [])
+        XCTAssertEqual(context.viewState.membersWithRole.count, 1)
+        XCTAssertFalse(context.viewState.membersWithRole.contains(firstUser))
+        XCTAssertFalse(context.viewState.hasChanges)
+    }
+    
+    func testDemoteToggledUser() {
+        testToggleUserOn()
+        guard let firstUser = context.viewState.membersToPromote.first else {
+            XCTFail("There should be a promoted member before we begin.")
+            return
+        }
+        
+        context.send(viewAction: .demoteMember(firstUser))
+        
+        XCTAssertEqual(context.viewState.membersToPromote, [])
+        XCTAssertEqual(context.viewState.membersToDemote, [])
+        XCTAssertEqual(context.viewState.membersWithRole.count, 1)
+        XCTAssertFalse(context.viewState.membersWithRole.contains(firstUser))
+        XCTAssertFalse(context.viewState.hasChanges)
+    }
+    
+    func testToggleModeratorOff() {
+        testInitialStateModerators()
+        guard let existingModerator = context.viewState.membersWithRole.first else {
+            XCTFail("There should be a member with the role before we begin.")
+            return
+        }
+        
+        context.send(viewAction: .toggleMember(existingModerator))
+        
+        XCTAssertEqual(context.viewState.membersToPromote, [])
+        XCTAssertEqual(context.viewState.membersToDemote, [existingModerator])
+        XCTAssertEqual(context.viewState.membersWithRole.count, 0)
+        XCTAssertFalse(context.viewState.membersWithRole.contains(existingModerator))
+        XCTAssertTrue(context.viewState.hasChanges)
+    }
+    
+    func testToggleModeratorOn() {
+        testToggleModeratorOff()
+        
+        guard let demotedMember = context.viewState.membersToDemote.first else {
+            XCTFail("There should be a member selected to demote before we begin.")
+            return
+        }
+        
+        context.send(viewAction: .toggleMember(demotedMember))
+        
+        XCTAssertEqual(context.viewState.membersToPromote, [])
+        XCTAssertEqual(context.viewState.membersToDemote, [])
+        XCTAssertEqual(context.viewState.membersWithRole.count, 1)
+        XCTAssertTrue(context.viewState.membersWithRole.contains(demotedMember))
+        XCTAssertFalse(context.viewState.hasChanges)
+    }
+    
+    func testDemoteModerator() {
+        testInitialStateModerators()
+        guard let existingModerator = context.viewState.membersWithRole.first else {
+            XCTFail("There should be a member with the role before we begin.")
+            return
+        }
+        
+        context.send(viewAction: .demoteMember(existingModerator))
+        
+        XCTAssertEqual(context.viewState.membersToPromote, [])
+        XCTAssertEqual(context.viewState.membersToDemote, [existingModerator])
+        XCTAssertEqual(context.viewState.membersWithRole.count, 0)
+        XCTAssertFalse(context.viewState.membersWithRole.contains(existingModerator))
+        XCTAssertTrue(context.viewState.hasChanges)
+    }
+    
+    func testSaveChanges() async throws {
+        setupRoomProxy()
+        viewModel = RoomChangeRolesScreenViewModel(mode: .moderator, roomProxy: roomProxy, userIndicatorController: UserIndicatorControllerMock())
+        
+        guard let firstUser = context.viewState.members.filter({ !context.viewState.isMemberSelected($0) }).first,
+              let existingModerator = context.viewState.membersWithRole.first else {
+            XCTFail("There should be a regular user and a moderator to begin with.")
+            return
+        }
+        
+        context.send(viewAction: .toggleMember(firstUser))
+        context.send(viewAction: .toggleMember(existingModerator))
+        context.send(viewAction: .save)
+        
+        try await Task.sleep(for: .milliseconds(100))
+        
+        XCTAssertTrue(roomProxy.updatePowerLevelsForUsersCalled)
+        XCTAssertEqual(roomProxy.updatePowerLevelsForUsersReceivedUpdates?.count, 2)
+        XCTAssertEqual(roomProxy.updatePowerLevelsForUsersReceivedUpdates?.contains(where: { $0.userID == existingModerator.id && $0.powerLevel == 0 }), true)
+        XCTAssertEqual(roomProxy.updatePowerLevelsForUsersReceivedUpdates?.contains(where: { $0.userID == firstUser.id && $0.powerLevel == 50 }), true)
+    }
+    
+    private func setupRoomProxy() {
+        roomProxy = RoomProxyMock(with: .init(members: .allMembersAsAdmin))
+    }
+}
