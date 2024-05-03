3d25eb790925662d64973dc129de8d53eb05ce77
Doug <douglase@element.io>
2024-05-03 10:21:57 +0100

    Update back button label.

--- ElementX/Sources/Screens/RoomScreen/View/RoomScreen.swift
+++ ElementX/Sources/Screens/RoomScreen/View/RoomScreen.swift
@@ -47,7 +47,7 @@ struct RoomScreen: View {
                     .background(Color.compound.bgCanvasDefault.ignoresSafeArea())
                     .environmentObject(context)
             }
-            .navigationTitle(L10n.commonRoom) // Hidden but used for back button text.
+            .navigationTitle(L10n.screenRoomTitle) // Hidden but used for back button text.
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarHidden(isNavigationBarHidden)
             .toolbar { toolbar }
