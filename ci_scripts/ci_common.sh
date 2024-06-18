f56eb3647b14bfd1dc62d96b963eb9ea6b81d663
Doug <douglase@element.io>
2024-06-18 11:56:23 +0100

    Try Ruby 3.0 on CI.

--- ci_scripts/ci_common.sh
+++ ci_scripts/ci_common.sh
@@ -12,11 +12,16 @@ setup_xcode_cloud_environment () {
     # Prevent installing dependencies in system directories
     echo 'export GEM_HOME=$HOME/.gem' >>~/.zshrc
     echo 'export PATH=$GEM_HOME/bin:$PATH' >>~/.zshrc
+    echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> ~/.zshrc
     echo 'export PATH="/Users/local/Library/Python/3.9/bin:$PATH"' >> ~/.zshrc
 
     export GEM_HOME=$HOME/.gem
     export PATH=$GEM_HOME/bin:$PATH
+    export PATH="/usr/local/opt/ruby/bin:$PATH"
     export PATH="/Users/local/Library/Python/3.9/bin:$PATH"
+
+    # Things don't work well on the default ruby version
+    brew install ruby
 
     gem install bundler
 
