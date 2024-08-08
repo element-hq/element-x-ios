0ad8f3cae8bc1a8cfd00625891cf6cf1dd4186c8
Stefan Ceriu <stefanc@matrix.org>
2024-08-08 14:11:33 +0300

    Fixes #3137 - Delay starting ElementCall until a sync response comes in (or a 5 second timeout) - this will ensure that the room state is up to date before sending any call related events

--- ElementX/Sources/Screens/CallScreen/View/CallScreen.swift
+++ ElementX/Sources/Screens/CallScreen/View/CallScreen.swift
@@ -22,12 +22,16 @@ struct CallScreen: View {
     @ObservedObject var context: CallScreenViewModel.Context
     
     var body: some View {
-        WebView(url: context.viewState.url, viewModelContext: context)
-            // This URL is stable, forces view reloads if this representable is ever reused for another url
-            .id(context.viewState.url)
-            .ignoresSafeArea(edges: .bottom)
-            .presentationDragIndicator(.visible)
-            .alert(item: $context.alertInfo)
+        if context.viewState.url == nil {
+            ProgressView()
+        } else {
+            WebView(url: context.viewState.url, viewModelContext: context)
+                // This URL is stable, forces view reloads if this representable is ever reused for another url
+                .id(context.viewState.url)
+                .ignoresSafeArea(edges: .bottom)
+                .presentationDragIndicator(.visible)
+                .alert(item: $context.alertInfo)
+        }
     }
 }
 
