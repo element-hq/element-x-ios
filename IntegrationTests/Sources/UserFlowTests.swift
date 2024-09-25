437f026e26d7cc7e89d686d6ccb27f428b470607
Stefan Ceriu <stefanc@matrix.org>
2024-09-25 19:28:59 +0300

    Revert "Integration tests: fix issue where segmented control entries are considered buttons"
    This reverts commit 220c8d1d2d14cd91c59b38c53352156fa3ad79cb.

--- IntegrationTests/Sources/UserFlowTests.swift
+++ IntegrationTests/Sources/UserFlowTests.swift
@@ -181,7 +181,7 @@ class UserFlowTests: XCTestCase {
         tapOnButton(A11yIdentifiers.roomDetailsScreen.people)
         
         // Open the first member's details. Loading members for big rooms can take a while.
-        let firstRoomMember = app.scrollViews.buttons.element(boundBy: 2) // First 2 are the segmented control
+        let firstRoomMember = app.scrollViews.buttons.firstMatch
         XCTAssertTrue(firstRoomMember.waitForExistence(timeout: 1000.0))
         firstRoomMember.tap()
         
