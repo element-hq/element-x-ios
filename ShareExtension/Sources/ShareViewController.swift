09d3bfb35e024559bc843e8f3a46e7a90ac2e048
Stefan Ceriu <stefanc@matrix.org>
2024-10-28 14:34:54 +0200

    Setup simple share extension

---
+++ ShareExtension/Sources/ShareViewController.swift
@@ -0,0 +1,16 @@
+//
+// Copyright 2024 New Vector Ltd.
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+// Please see LICENSE in the repository root for full details.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
