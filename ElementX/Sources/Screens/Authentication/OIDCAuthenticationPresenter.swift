7d461d81ebdc121ae52cbfd9bf8aef5146e5506f
Doug <douglase@element.io>
2024-10-29 09:34:20 +0000

    Simplify OIDCAuthenticationPresenter now it doesn't need to handle universal links.

--- ElementX/Sources/Screens/Authentication/OIDCAuthenticationPresenter.swift
+++ ElementX/Sources/Screens/Authentication/OIDCAuthenticationPresenter.swift
@@ -14,16 +14,6 @@ class OIDCAuthenticationPresenter: NSObject {
     private let oidcRedirectURL: URL
     private let presentationAnchor: UIWindow
     
-    /// The data required to complete a request.
-    struct Request {
-        let session: ASWebAuthenticationSession
-        let oidcData: OIDCAuthorizationDataProxy
-        let continuation: CheckedContinuation<Result<UserSessionProtocol, AuthenticationServiceError>, Never>
-    }
-    
-    /// The current request in progress. This is a single use value and will be moved on access.
-    @Consumable private var request: Request?
-    
     init(authenticationService: AuthenticationServiceProtocol, oidcRedirectURL: URL, presentationAnchor: UIWindow) {
         self.authenticationService = authenticationService
         self.oidcRedirectURL = oidcRedirectURL
@@ -33,67 +23,35 @@ class OIDCAuthenticationPresenter: NSObject {
     
     /// Presents a web authentication session for the supplied data.
     func authenticate(using oidcData: OIDCAuthorizationDataProxy) async -> Result<UserSessionProtocol, AuthenticationServiceError> {
-        await withCheckedContinuation { continuation in
-            let session = ASWebAuthenticationSession(url: oidcData.url,
-                                                     callback: .oidcRedirectURL(oidcRedirectURL)) { [weak self] url, error in
-                guard let self else { return }
-                
-                guard let url else {
-                    // Check for user cancellation to avoid showing an alert in that instance.
-                    if let nsError = error as? NSError,
-                       nsError.domain == ASWebAuthenticationSessionErrorDomain,
-                       nsError.code == ASWebAuthenticationSessionError.canceledLogin.rawValue {
-                        self.completeAuthentication(throwing: .oidcError(.userCancellation))
-                        return
-                    }
-                    
-                    self.completeAuthentication(throwing: .oidcError(.unknown))
-                    return
-                }
-                
-                completeAuthentication(callbackURL: url)
+        let (url, error) = await withCheckedContinuation { continuation in
+            let session = ASWebAuthenticationSession(url: oidcData.url, callback: .oidcRedirectURL(oidcRedirectURL)) { url, error in
+                continuation.resume(returning: (url, error))
             }
             
             session.prefersEphemeralWebBrowserSession = false
             session.presentationContextProvider = self
             
-            request = Request(session: session, oidcData: oidcData, continuation: continuation)
-            
             session.start()
         }
-    }
-    
-    /// Completes the authentication by exchanging the callback URL for a user session.
-    private func completeAuthentication(callbackURL: URL) {
-        guard let request else {
-            MXLog.error("Failed to complete authentication. Missing request.")
-            return
-        }
         
-        if callbackURL.scheme?.starts(with: "http") == true {
-            request.session.cancel()
-        }
-        
-        Task {
-            switch await authenticationService.loginWithOIDCCallback(callbackURL, data: request.oidcData) {
-            case .success(let userSession):
-                request.continuation.resume(returning: .success(userSession))
-            case .failure(let error):
-                request.continuation.resume(returning: .failure(error))
+        guard let url else {
+            // Check for user cancellation to avoid showing an alert in that instance.
+            if let nsError = error as? NSError,
+               nsError.domain == ASWebAuthenticationSessionErrorDomain,
+               nsError.code == ASWebAuthenticationSessionError.canceledLogin.rawValue {
+                await authenticationService.abortOIDCLogin(data: oidcData)
+                return .failure(.oidcError(.userCancellation))
             }
-        }
-    }
-    
-    /// Aborts the authentication with the supplied error.
-    private func completeAuthentication(throwing error: AuthenticationServiceError) {
-        guard let request else {
-            MXLog.error("Failed to throw authentication error. Missing request.")
-            return
+            
+            await authenticationService.abortOIDCLogin(data: oidcData)
+            return .failure(.oidcError(.unknown))
         }
         
-        Task {
-            await authenticationService.abortOIDCLogin(data: request.oidcData)
-            request.continuation.resume(returning: .failure(error))
+        switch await authenticationService.loginWithOIDCCallback(url, data: oidcData) {
+        case .success(let userSession):
+            return .success(userSession)
+        case .failure(let error):
+            return .failure(error)
         }
     }
 }
