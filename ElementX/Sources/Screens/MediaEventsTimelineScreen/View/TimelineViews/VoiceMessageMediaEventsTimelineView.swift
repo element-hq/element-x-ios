37c45f8012954418c059c3c6152a38d9b65a54ee
Stefan Ceriu <stefanc@matrix.org>
2024-12-12 16:01:02 +0200

    Use different views for files and voices messages, reuse the timeline content but only keep the bubble background instead of the whole TimelineStyler.

--- ElementX/Sources/Screens/MediaEventsTimelineScreen/View/TimelineViews/VoiceMessageMediaEventsTimelineView.swift
+++ ElementX/Sources/Screens/MediaEventsTimelineScreen/View/TimelineViews/VoiceMessageMediaEventsTimelineView.swift
@@ -5,60 +5,27 @@
 // Please see LICENSE in the repository root for full details.
 //
 
-import Foundation
+import Compound
 import SwiftUI
 
-struct VoiceMessageRoomTimelineView: View {
+struct VoiceMessageMediaEventsTimelineView: View {
     let timelineItem: VoiceMessageRoomTimelineItem
     let playerState: AudioPlayerState
     
     var body: some View {
-        TimelineStyler(timelineItem: timelineItem) {
-            VoiceMessageRoomTimelineContent(timelineItem: timelineItem,
-                                            playerState: playerState)
-        }
+        VoiceMessageRoomTimelineContent(timelineItem: timelineItem,
+                                        playerState: playerState)
+            .accessibilityLabel(L10n.commonVoiceMessage)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .bubbleBackground(timelineItem: timelineItem,
+                              insets: .init(top: 8, leading: 12, bottom: 8, trailing: 12),
+                              color: .compound.bgSubtleSecondary)
     }
 }
 
-struct VoiceMessageRoomTimelineContent: View {
-    @Environment(\.timelineContext) private var context
-    @State private var resumePlaybackAfterScrubbing = false
-    
-    let timelineItem: VoiceMessageRoomTimelineItem
-    let playerState: AudioPlayerState
-    
-    var body: some View {
-        VoiceMessageRoomPlaybackView(playerState: playerState,
-                                     onPlayPause: onPlaybackPlayPause,
-                                     onSeek: { onPlaybackSeek($0) },
-                                     onScrubbing: { onPlaybackScrubbing($0) })
-            .fixedSize(horizontal: false, vertical: true)
-    }
-    
-    private func onPlaybackPlayPause() {
-        context?.send(viewAction: .handleAudioPlayerAction(.playPause(itemID: timelineItem.id)))
-    }
-    
-    private func onPlaybackSeek(_ progress: Double) {
-        context?.send(viewAction: .handleAudioPlayerAction(.seek(itemID: timelineItem.id, progress: progress)))
-    }
-    
-    private func onPlaybackScrubbing(_ dragging: Bool) {
-        if dragging {
-            if playerState.playbackState == .playing {
-                resumePlaybackAfterScrubbing = true
-                context?.send(viewAction: .handleAudioPlayerAction(.playPause(itemID: timelineItem.id)))
-            }
-        } else {
-            if resumePlaybackAfterScrubbing {
-                context?.send(viewAction: .handleAudioPlayerAction(.playPause(itemID: timelineItem.id)))
-                resumePlaybackAfterScrubbing = false
-            }
-        }
-    }
-}
+// MARK: - Content
 
-struct VoiceMessageRoomTimelineView_Previews: PreviewProvider, TestablePreview {
+struct VoiceMessageMediaEventsTimelineView_Previews: PreviewProvider, TestablePreview {
     static let viewModel = TimelineViewModel.mock
     static let timelineItemIdentifier = TimelineItemIdentifier.randomEvent
     static let voiceRoomTimelineItem = VoiceMessageRoomTimelineItem(id: timelineItemIdentifier,
@@ -82,12 +49,12 @@ struct VoiceMessageRoomTimelineView_Previews: PreviewProvider, TestablePreview {
                                               progress: 0.4)
     
     static var previews: some View {
-        body.environmentObject(viewModel.context)
-            .previewDisplayName("Bubble")
+        body
+            .environmentObject(viewModel.context)
     }
     
     static var body: some View {
-        VoiceMessageRoomTimelineView(timelineItem: voiceRoomTimelineItem, playerState: playerState)
+        VoiceMessageMediaEventsTimelineView(timelineItem: voiceRoomTimelineItem, playerState: playerState)
             .fixedSize(horizontal: false, vertical: true)
     }
 }
