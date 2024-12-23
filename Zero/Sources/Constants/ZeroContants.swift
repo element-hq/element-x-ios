4df7596b7a468bce9177cca189f755530b461c6f
Shahroz <shrozali@outlook.com>
2024-12-23 13:42:22 +0500

    Added script to fetch wallet connect project id from xcode cloud

--- Zero/Sources/Constants/ZeroContants.swift
+++ Zero/Sources/Constants/ZeroContants.swift
@@ -16,12 +16,12 @@ struct DevServer: AppServer {
     let matrixHomeServerUrl = "https://zero-synapse-development-db365bf96189.herokuapp.com"
     let matrixHomeServerPostfix = "zero-synapse-development.zer0.io"
     let zeroRootUrl = "https://zos-api-development-fb2c513ffa60.herokuapp.com/"
-    let walletConnectProjectId = "31b01edcaf38a5622417a3cb6d6a6142"
+    let walletConnectProjectId = ZeroSecrets.walletConnectProjectId
 }
 
 struct ProdServer: AppServer {
     let matrixHomeServerUrl = "https://zos-home-2-e24b9412096f.herokuapp.com"
     let matrixHomeServerPostfix = "zos-home-2.zero.tech"
     let zeroRootUrl = "https://zosapi.zero.tech/"
-    let walletConnectProjectId = "31b01edcaf38a5622417a3cb6d6a6142"
+    let walletConnectProjectId = ZeroSecrets.walletConnectProjectId
 }
---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Stage: PRE-Xcode Build is activated .... "
+
+# Move to the place where the scripts are located.
+# This is important because the position of the subsequently mentioned files depend of this origin.
+cd ../ci_scripts || exit 1
+
+# Write a JSON File containing all the environment variables and secrets.
+printf "{\"WALLET_CONNECT_PROJECT_ID\":\"%s\"}" "$WALLET_CONNECT_PROJECT_ID" >> ../Zero/SupportingFiles/zero_secrets.json
+
+echo "Wrote zero_secrets.json file."
+
+echo "Stage: PRE-Xcode Build is DONE .... "
+
+exit 0
