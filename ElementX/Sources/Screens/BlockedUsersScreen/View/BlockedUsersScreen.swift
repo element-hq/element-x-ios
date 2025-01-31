c4521033b94532cad51653b880c0cef881ab09fe
Doug <douglase@element.io>
2025-01-31 16:13:19 +0000

    Fix missing user ID.

--- ElementX/Sources/Screens/BlockedUsersScreen/View/BlockedUsersScreen.swift
+++ ElementX/Sources/Screens/BlockedUsersScreen/View/BlockedUsersScreen.swift
@@ -32,7 +32,9 @@ struct BlockedUsersScreen: View {
         } else {
             Form {
                 ForEach(context.viewState.blockedUsers, id: \.self) { user in
-                    ListRow(label: .avatar(title: user.displayName ?? user.userID, icon: avatar(for: user)),
+                    ListRow(label: .avatar(title: user.displayName ?? user.userID,
+                                           description: user.displayName != nil ? user.userID : nil,
+                                           icon: avatar(for: user)),
                             details: .isWaiting(context.viewState.processingUserID == user.userID),
                             kind: .button { context.send(viewAction: .unblockUser(user)) })
                 }
