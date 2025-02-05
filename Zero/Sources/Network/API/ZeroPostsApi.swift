19368214068194276a311269a5dcacbad1e44202
shahroz-wilder <shrozali@outlook.com>
2025-02-06 02:01:47 +0500

    Added post details and post replies APIs and models

--- Zero/Sources/Network/API/ZeroPostsApi.swift
+++ Zero/Sources/Network/API/ZeroPostsApi.swift
@@ -10,6 +10,8 @@ import Foundation
 
 protocol ZeroPostsApiProtocol {
     func fetchPosts(limit: Int, skip: Int) async throws -> Result<[ZPost], Error>
+    func fetchPostDetails(postId: String) async throws -> Result<ZPost, Error>
+    func fetchPostReplies(postId: String, limit: Int, skip: Int) async throws -> Result<[ZPost], Error>
 }
 
 class ZeroPostsApi: ZeroPostsApiProtocol {
@@ -44,9 +46,61 @@ class ZeroPostsApi: ZeroPostsApiProtocol {
         }
     }
     
+    func fetchPostDetails(postId: String) async throws -> Result<ZPost, any Error> {
+        let parameters: [String: Any] = [
+            "include_replies": "true",
+            "include_meows": "true"
+        ]
+        let requestUrl = FeedEndPoints.postDetailsEndPoint
+            .replacingOccurrences(of: FeedConstants.feed_id_path_param, with: postId)
+        let result: Result<ZPostDetails, Error> = try await APIManager
+            .shared
+            .authorisedRequest(requestUrl,
+                               method: .get,
+                               appSettings: appSettings,
+                               parameters: parameters,
+                               encoding: URLEncoding.queryString)
+        switch result {
+        case .success(let postDetails):
+            return .success(postDetails.post)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+    func fetchPostReplies(postId: String, limit: Int, skip: Int) async throws -> Result<[ZPost], any Error> {
+        let parameters: [String: Any] = [
+            "limit": limit,
+            "skip": skip,
+            "include_replies": "true",
+            "include_meows": "true"
+        ]
+        let requestUrl = FeedEndPoints.postRepliesEndPoint
+            .replacingOccurrences(of: FeedConstants.feed_id_path_param, with: postId)
+        let result: Result<ZPostReplies, Error> = try await APIManager
+            .shared
+            .authorisedRequest(requestUrl,
+                               method: .get,
+                               appSettings: appSettings,
+                               parameters: parameters,
+                               encoding: URLEncoding.queryString)
+        switch result {
+        case .success(let postReplies):
+            return .success(postReplies.replies)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
     // MARK: - Constants
     
     private enum FeedEndPoints {
         static let postsEndPoint = "\(ZeroContants.appServer.zeroRootUrl)api/v2/posts"
+        static let postDetailsEndPoint = "\(ZeroContants.appServer.zeroRootUrl)api/v2/posts/\(FeedConstants.feed_id_path_param)"
+        static let postRepliesEndPoint = "\(ZeroContants.appServer.zeroRootUrl)api/v2/posts/\(FeedConstants.feed_id_path_param)/replies"
+    }
+    
+    private enum FeedConstants {
+        static let feed_id_path_param = "{feed_id}"
     }
 }
