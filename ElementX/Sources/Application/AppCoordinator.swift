37f353bc3122ee3e3a7c01638941536c923cf843
Stefan Ceriu <stefanc@matrix.org>
2025-02-25 11:06:15 +0200

    Bump the RustSDK to v25.2.25, address breaking changes from matrix-org/matrix-rust-sdk/pull/4674

--- ElementX/Sources/Application/AppCoordinator.swift
+++ ElementX/Sources/Application/AppCoordinator.swift
@@ -591,7 +591,7 @@ class AppCoordinator: AppCoordinatorProtocol, AuthenticationFlowCoordinatorDeleg
         
         Task {
             // First log out from the server
-            let accountLogoutURL = await userSession.clientProxy.logout()
+            await userSession.clientProxy.logout()
             
             // Regardless of the result, clear user data
             userSessionStore.logout(userSession: userSession)
@@ -604,13 +604,7 @@ class AppCoordinator: AppCoordinatorProtocol, AuthenticationFlowCoordinatorDeleg
             ServiceLocator.shared.analytics.resetConsentState()
             
             stateMachine.processEvent(.completedSigningOut)
-            
-            // Handle OIDC's RP-Initiated Logout if needed. Don't fallback to an ASWebAuthenticationSession
-            // as it looks weird to show an alert to the user asking them to sign in to their provider.
-            if let accountLogoutURL, UIApplication.shared.canOpenURL(accountLogoutURL) {
-                await UIApplication.shared.open(accountLogoutURL)
-            }
-            
+                       
             hideLoadingIndicator()
         }
     }
