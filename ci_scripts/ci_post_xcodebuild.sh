7a8f44f607cb380766eae17aecae2e900b333f6a
Mauro Romito <mauro.romito@element.io>
2025-03-07 10:45:57 +0100

    fetch unshallow only in post-clone

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,6 +4,9 @@ source ci_common.sh
 
 setup_xcode_cloud_environment
 
+# Xcode Cloud shallow clones the repo. We need to deepen it to fetch tags, commit history and be able to rebase main on develop at the end of releases.
+fetch_unshallow_repository
+
 # Upload dsyms no matter the workflow
 # Perform this step before releasing to github in case it fails.
 bundle exec fastlane upload_dsyms_to_sentry dsym_path:"$CI_ARCHIVE_PATH/dSYMs"
