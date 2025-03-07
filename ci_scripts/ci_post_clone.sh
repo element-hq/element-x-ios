4b7cf3c6a5d0b405a7c3ed84597e836cef9f874d
Doug <6060466+pixlwave@users.noreply.github.com>
2025-03-07 13:47:11 +0000

    Rename the Enterprise variant to Pro. (#3877)

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,9 +8,9 @@ install_xcode_cloud_brew_dependencies
 
 if [ "$CI_WORKFLOW" = "Nightly" ]; then
     bundle exec fastlane config_nightly build_number:"$CI_BUILD_NUMBER"
-elif [ "$CI_WORKFLOW" = "Enterprise" ]; then
-    # Xcode Cloud automatically fetches the submodule.
-    bundle exec fastlane config_enterprise
+elif [ "$CI_WORKFLOW" = "Element Pro" ]; then
+    # Xcode Cloud automatically fetches the submodules.
+    bundle exec fastlane config_element_pro
 else
     bundle exec fastlane config_production
 fi
