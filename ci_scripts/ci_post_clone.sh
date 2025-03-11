0b96b4b986423ab3dd0996297a6665eb2f63cc3a
shahroz-wilder <shrozali@outlook.com>
2025-03-11 15:21:07 +0500

    Reverted ci_post_clone.sh to previous script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,13 +4,7 @@ source ci_common.sh
 
 setup_xcode_cloud_environment
 
-install_xcode_cloud_brew_dependencies
-
-if [ "$CI_WORKFLOW" = "Nightly" ]; then
-    bundle exec fastlane config_nightly build_number:"$CI_BUILD_NUMBER"
-elif [ "$CI_WORKFLOW" = "Element Pro" ]; then
-    # Xcode Cloud automatically fetches the submodules.
-    bundle exec fastlane config_element_pro
-else
-    bundle exec fastlane config_production
-fi
+# Add debugging information
+echo "Current directory: $(pwd)"
+echo "Contents of current directory:"
+ls -la
