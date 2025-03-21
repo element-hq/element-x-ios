5aca98158cd19aadb3aba62c5e54f16f187c0573
shahroz-wilder <shrozali@outlook.com>
2025-03-21 23:11:19 +0500

    fix: updated home tab view behavior

--- ElementX/Sources/Screens/HomeScreen/View/HomeTabView.swift
+++ ElementX/Sources/Screens/HomeScreen/View/HomeTabView.swift
@@ -13,110 +13,43 @@ enum HomeTab: CaseIterable {
     case feed
     case notifications
     case myFeed
-    
-    static func from(index: Int) -> HomeTab {
-        return Self.allCases.indices.contains(index) ? Self.allCases[index] : .chat // by-default `chat` tab is selected
-    }
 }
 
-struct HomeTabView<Content1: View, Content2: View, Content3: View, Content4: View, Content5: View>: View {
-    @State private var selectedTab = 0
+struct HomeTabView<Content: View>: View {
+    @State private var selectedTab: HomeTab = .chat
     
-    let chatTabContent: Content1
-    let channelTabContent: Content2
-    let notificationsTabContent: Content3
-    let homeTabContent: Content4
-    let myFeedTabContent: Content5
-    
-    let onTabSelected: (Int, HomeTab) -> Void
+    let tabContent: (HomeTab) -> Content
+    let onTabSelected: (HomeTab) -> Void
     
     private let tabs = [
-        (
-            title: "Chat",
-            icon: Asset.Images.homeTabChatIcon
-        ),
-        (
-            title: "Channels",
-            icon: Asset.Images.homeTabExplorerIcon
-        ),
-        (
-            title: "Feed",
-            icon: Asset.Images.homeTabFeedIcon
-        ),
-        (
-            title: "Notifications",
-            icon: Asset.Images.homeTabNotificationsIcon
-        ),
-        (
-            title: "My Feed",
-            icon: Asset.Images.homeTabProfileIcon
-        )
+        (title: "Chat", icon: Asset.Images.homeTabChatIcon, tab: HomeTab.chat),
+        (title: "Channels", icon: Asset.Images.homeTabExplorerIcon, tab: HomeTab.channels),
+        (title: "Feed", icon: Asset.Images.homeTabFeedIcon, tab: HomeTab.feed),
+        (title: "Notifications", icon: Asset.Images.homeTabNotificationsIcon, tab: HomeTab.notifications),
+        (title: "My Feed", icon: Asset.Images.homeTabProfileIcon, tab: HomeTab.myFeed)
     ]
     
-    init(@ViewBuilder chatTabContent: () -> Content1,
-         @ViewBuilder channelTabContent: () -> Content2,
-         @ViewBuilder notificationsTabContent: () -> Content3,
-         @ViewBuilder homeTabContent: () -> Content4,
-         @ViewBuilder myFeedTabContent: () -> Content5,
-         onTabSelected: @escaping (Int, HomeTab) -> Void) {
-        self.chatTabContent = chatTabContent()
-        self.channelTabContent = channelTabContent()
-        self.notificationsTabContent = notificationsTabContent()
-        self.homeTabContent = homeTabContent()
-        self.myFeedTabContent = myFeedTabContent()
+    init(@ViewBuilder tabContent: @escaping (HomeTab) -> Content,
+         onTabSelected: @escaping (HomeTab) -> Void) {
+        self.tabContent = tabContent
         self.onTabSelected = onTabSelected
     }
     
     var body: some View {
-        VStack(spacing: 0) {
-            // Content View
-            Group {
-                switch selectedTab {
-                case 1:
-                    channelTabContent
-                case 2:
-                    homeTabContent
-                case 3:
-                    notificationsTabContent
-                case 4:
-                    myFeedTabContent
-                default:
-                    chatTabContent
-                }
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            
-            // Custom Tab Bar
-            VStack(spacing: 0) {
-                Divider()
-                HStack(spacing: 0) {
-                    ForEach(0..<tabs.count, id: \.self) { index in
-                        tabButton(
-                            title: tabs[index].title,
-                            icon: tabs[index].icon,
-                            isSelected: selectedTab == index,
-                            index: index
-                        )
+        TabView(selection: $selectedTab) {
+            ForEach(tabs, id: \.tab) { tabInfo in
+                tabContent(tabInfo.tab)
+                    .tabItem {
+                        Image(asset: tabInfo.icon)
+                            .foregroundStyle(tabInfo.tab == selectedTab ? .zero.bgAccentRest : .compound.iconSecondary)
+                        //Text(tabInfo.title)
                     }
-                }
+                    .tag(tabInfo.tab)
             }
-            .background(.ultraThickMaterial)
         }
-        .onChange(of: selectedTab) { _, newValue in
-            self.onTabSelected(newValue, HomeTab.from(index: newValue))
-        }
-    }
-    
-    // Tab Button
-    private func tabButton(title: String, icon: ImageAsset, isSelected: Bool, index: Int) -> some View {
-        Button(action: { selectedTab = index }) {
-            VStack {
-                Image(asset: icon)
-                    .font(.system(size: 18))
-                    .foregroundStyle(isSelected ? .zero.bgAccentRest : .compound.textSecondary)
-            }
-            .frame(maxWidth: .infinity) // Equal width for all buttons
-            .padding(.vertical, 10)
+        .accentColor(.zero.bgAccentRest)
+        .onChange(of: selectedTab) { _, newTab in
+            onTabSelected(newTab)
         }
     }
 }
