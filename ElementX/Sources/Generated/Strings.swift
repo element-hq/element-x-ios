a4eeb6652730ac9897bca70211c84c0878999021
Mauro Romito <mauro.romito@element.io>
2025-03-27 16:34:33 +0100

    a11y: add profile picture accessibility label

--- ElementX/Sources/Generated/Strings.swift
+++ ElementX/Sources/Generated/Strings.swift
@@ -10,6 +10,8 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
+  /// Avatar
+  internal static var a11yAvatar: String { return L10n.tr("Localizable", "a11y_avatar") }
   /// Delete
   internal static var a11yDelete: String { return L10n.tr("Localizable", "a11y_delete") }
   /// Plural format key: "%#@COUNT@"
