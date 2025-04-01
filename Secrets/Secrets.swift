6a8f35db9c98a765950b7d146679edd4f22e82a0
Doug <6060466+pixlwave@users.noreply.github.com>
2025-04-01 16:14:05 +0100

    Make the secrets optional. (#3966)

--- Secrets/Secrets.swift
+++ Secrets/Secrets.swift
@@ -1,19 +1,8 @@
-// swiftlint:disable all
-// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+enum Secrets {
+    static let sentryDSN: String? = "https://sentry.localhost"
+    static let postHogHost: String? = "https://posthog.localhost"
+    static let postHogAPIKey: String? = "your_key"
+    static let rageshakeServerURL: String? = "https://rageshake.localhost"
+    static let mapLibreAPIKey: String? = "your_key"
 
-import Foundation
-
-// swiftlint:disable superfluous_disable_command
-// swiftlint:disable file_length
-
-// MARK: - YAML Files
-
-// swiftlint:disable identifier_name line_length number_separator type_body_length
-internal enum Secrets {
-  internal static let mapLibreAPIKey: String = "your_key"
-  internal static let postHogAPIKey: String = "your_key"
-  internal static let postHogHost: String = "https://posthog.localhost"
-  internal static let rageshakeServerURL: String = "https://rageshake.localhost"
-  internal static let sentryDSN: String = "https://sentry.localhost"
 }
-// swiftlint:enable identifier_name line_length number_separator type_body_length
