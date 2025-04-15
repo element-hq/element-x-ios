38b2591e6029eacd556f63417c9db2e009528fbb
Doug <douglase@element.io>
2025-04-15 17:17:05 +0100

    Refactor SecureBackupControllerListener into SDKListener and use it everywhere.

---
+++ ElementX/Sources/Other/SDKListener.swift
@@ -0,0 +1,101 @@
+//
+// Copyright 2025 New Vector Ltd.
+//
+// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+// Please see LICENSE files in the repository root for full details.
+//
+
+import Foundation
+import MatrixRustSDK
+
+/// A helper class that can be passed as this listener for SDK callbacks.
+///
+/// To use this you'll need to add a conformance to the required listener
+/// protocol with a specialisation for the type it listens for.
+final class SDKListener<T> {
+    private let onUpdateClosure: (T) -> Void
+    
+    /// Creates a new listener.
+    /// - Parameter onUpdateClosure: A closure that will be called whenever a new value is available.
+    init(_ onUpdateClosure: @escaping (T) -> Void) {
+        self.onUpdateClosure = onUpdateClosure
+    }
+}
+
+// MARK: QRCodeLoginService
+
+extension SDKListener: QrLoginProgressListener where T == QrLoginProgress {
+    func onUpdate(state: QrLoginProgress) { onUpdateClosure(state) }
+}
+
+// MARK: ClientProxy
+
+extension SDKListener: SyncServiceStateObserver where T == SyncServiceState {
+    func onUpdate(state: SyncServiceState) { onUpdateClosure(state) }
+}
+
+extension SDKListener: RoomListServiceStateListener where T == RoomListServiceState {
+    func onUpdate(state: RoomListServiceState) { onUpdateClosure(state) }
+}
+
+extension SDKListener: RoomListServiceSyncIndicatorListener where T == RoomListServiceSyncIndicator {
+    func onUpdate(syncIndicator: RoomListServiceSyncIndicator) { onUpdateClosure(syncIndicator) }
+}
+
+extension SDKListener: VerificationStateListener where T == VerificationState {
+    func onUpdate(status: VerificationState) { onUpdateClosure(status) }
+}
+
+// MARK: SecureBackupController
+
+extension SDKListener: BackupStateListener where T == BackupState {
+    func onUpdate(status: BackupState) { onUpdateClosure(status) }
+}
+
+extension SDKListener: RecoveryStateListener where T == RecoveryState {
+    func onUpdate(status: RecoveryState) { onUpdateClosure(status) }
+}
+
+extension SDKListener: EnableRecoveryProgressListener where T == EnableRecoveryProgress {
+    func onUpdate(status: EnableRecoveryProgress) { onUpdateClosure(status) }
+}
+
+extension SDKListener: BackupSteadyStateListener where T == BackupUploadState {
+    func onUpdate(status: BackupUploadState) { onUpdateClosure(status) }
+}
+
+// MARK: RoomSummaryProvider
+
+extension SDKListener: RoomListEntriesListener where T == [RoomListEntriesUpdate] {
+    func onUpdate(roomEntriesUpdate: [RoomListEntriesUpdate]) { onUpdateClosure(roomEntriesUpdate) }
+}
+
+extension SDKListener: RoomListLoadingStateListener where T == RoomListLoadingState {
+    func onUpdate(state: RoomListLoadingState) { onUpdateClosure(state) }
+}
+
+// MARK: TimelineProxy
+
+extension SDKListener: PaginationStatusListener where T == RoomPaginationStatus {
+    func onUpdate(status: RoomPaginationStatus) { onUpdateClosure(status) }
+}
+
+extension SDKListener: ProgressWatcher where T == Double {
+    func transmissionProgress(progress: TransmissionProgress) {
+        DispatchQueue.main.async { [weak self] in
+            self?.onUpdateClosure(Double(progress.current) / Double(progress.total))
+        }
+    }
+}
+
+// MARK: TimelineProvider
+
+extension SDKListener: TimelineListener where T == [TimelineDiff] {
+    func onUpdate(diff: [TimelineDiff]) { onUpdateClosure(diff) }
+}
+
+// MARK: RoomDirectorySearchProxy
+
+extension SDKListener: RoomDirectorySearchEntriesListener where T == [RoomDirectorySearchEntryUpdate] {
+    func onUpdate(roomEntriesUpdate: [RoomDirectorySearchEntryUpdate]) { onUpdateClosure(roomEntriesUpdate) }
+}
--- ElementX/Sources/Services/Client/ClientProxy.swift
+++ ElementX/Sources/Services/Client/ClientProxy.swift
@@ -192,7 +192,7 @@ class ClientProxy: ClientProxyProtocol {
         
         await updateVerificationState(client.encryption().verificationState())
         
-        verificationStateListenerTaskHandle = client.encryption().verificationStateListener(listener: VerificationStateListenerProxy { [weak self] verificationState in
+        verificationStateListenerTaskHandle = client.encryption().verificationStateListener(listener: SDKListener { [weak self] verificationState in
             Task { await self?.updateVerificationState(verificationState) }
         })
         
@@ -838,7 +838,7 @@ class ClientProxy: ClientProxyProtocol {
     }
     
     private func createSyncServiceStateObserver(_ syncService: SyncService) -> TaskHandle {
-        syncService.state(listener: SyncServiceStateObserverProxy { [weak self] state in
+        syncService.state(listener: SDKListener { [weak self] state in
             guard let self else { return }
             
             MXLog.info("Received sync service update: \(state)")
@@ -856,7 +856,7 @@ class ClientProxy: ClientProxyProtocol {
     }
 
     private func createRoomListServiceObserver(_ roomListService: RoomListService) -> TaskHandle {
-        roomListService.state(listener: RoomListStateListenerProxy { [weak self] state in
+        roomListService.state(listener: SDKListener { [weak self] state in
             guard let self else { return }
             
             MXLog.info("Received room list update: \(state)")
@@ -877,7 +877,7 @@ class ClientProxy: ClientProxyProtocol {
     }
     
     private func createRoomListLoadingStateUpdateObserver(_ roomListService: RoomListService) -> TaskHandle {
-        roomListService.syncIndicator(delayBeforeShowingInMs: 1000, delayBeforeHidingInMs: 0, listener: RoomListServiceSyncIndicatorListenerProxy { [weak self] state in
+        roomListService.syncIndicator(delayBeforeShowingInMs: 1000, delayBeforeHidingInMs: 0, listener: SDKListener { [weak self] state in
             guard let self else { return }
             
             switch state {
@@ -1036,54 +1036,6 @@ extension ClientProxy: MediaLoaderProtocol {
     
     func loadMediaFileForSource(_ source: MediaSourceProxy, filename: String?) async throws -> MediaFileHandleProxy {
         try await mediaLoader.loadMediaFileForSource(source, filename: filename)
-    }
-}
-
-private class SyncServiceStateObserverProxy: SyncServiceStateObserver {
-    private let onUpdateClosure: (SyncServiceState) -> Void
-
-    init(onUpdateClosure: @escaping (SyncServiceState) -> Void) {
-        self.onUpdateClosure = onUpdateClosure
-    }
-
-    func onUpdate(state: SyncServiceState) {
-        onUpdateClosure(state)
-    }
-}
-
-private class RoomListStateListenerProxy: RoomListServiceStateListener {
-    private let onUpdateClosure: (RoomListServiceState) -> Void
-
-    init(onUpdateClosure: @escaping (RoomListServiceState) -> Void) {
-        self.onUpdateClosure = onUpdateClosure
-    }
-
-    func onUpdate(state: RoomListServiceState) {
-        onUpdateClosure(state)
-    }
-}
-
-private class RoomListServiceSyncIndicatorListenerProxy: RoomListServiceSyncIndicatorListener {
-    private let onUpdateClosure: (RoomListServiceSyncIndicator) -> Void
-
-    init(onUpdateClosure: @escaping (RoomListServiceSyncIndicator) -> Void) {
-        self.onUpdateClosure = onUpdateClosure
-    }
-    
-    func onUpdate(syncIndicator: RoomListServiceSyncIndicator) {
-        onUpdateClosure(syncIndicator)
-    }
-}
-
-private class VerificationStateListenerProxy: VerificationStateListener {
-    private let onUpdateClosure: (VerificationState) -> Void
-
-    init(onUpdateClosure: @escaping (VerificationState) -> Void) {
-        self.onUpdateClosure = onUpdateClosure
-    }
-    
-    func onUpdate(status: VerificationState) {
-        onUpdateClosure(status)
     }
 }
 
