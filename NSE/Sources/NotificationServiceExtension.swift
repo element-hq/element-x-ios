1a3f3dd8a88f0835e2c51e9741d48d23e4e400bc
Stefan Ceriu <stefanc@matrix.org>
2025-04-17 13:55:47 +0300

    Remove unused NSE Task extension

--- NSE/Sources/NotificationServiceExtension.swift
+++ NSE/Sources/NotificationServiceExtension.swift
@@ -269,20 +269,3 @@ class NotificationServiceExtension: UNNotificationServiceExtension {
         case unsupportedShouldDiscard
     }
 }
-
-// https://stackoverflow.com/a/77300959/730924
-private extension Task where Failure == Error {
-    /// Performs an async task in a sync context.
-    ///
-    /// - Note: This function blocks the thread until the given operation is finished. The caller is responsible for managing multithreading.
-    static func synchronous(priority: TaskPriority? = nil, operation: @escaping @Sendable () async throws -> Success) {
-        let semaphore = DispatchSemaphore(value: 0)
-
-        Task(priority: priority) {
-            defer { semaphore.signal() }
-            return try await operation()
-        }
-
-        semaphore.wait()
-    }
-}
