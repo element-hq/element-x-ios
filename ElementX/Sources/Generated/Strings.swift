a1802f89e341620fd2c0e17b8243abaaac62517d
pixlwave <6060466+pixlwave@users.noreply.github.com>
2025-04-28 03:25:12 +0000

    Translations update

--- ElementX/Sources/Generated/Strings.swift
+++ ElementX/Sources/Generated/Strings.swift
@@ -2670,6 +2670,14 @@ internal enum L10n {
   internal static func stateEventRoomBanByYou(_ p1: Any) -> String {
     return L10n.tr("Localizable", "state_event_room_ban_by_you", String(describing: p1))
   }
+  /// You banned %1$@: %2$@
+  internal static func stateEventRoomBanByYouWithReason(_ p1: Any, _ p2: Any) -> String {
+    return L10n.tr("Localizable", "state_event_room_ban_by_you_with_reason", String(describing: p1), String(describing: p2))
+  }
+  /// %1$@ banned %2$@: %3$@
+  internal static func stateEventRoomBanWithReason(_ p1: Any, _ p2: Any, _ p3: Any) -> String {
+    return L10n.tr("Localizable", "state_event_room_ban_with_reason", String(describing: p1), String(describing: p2), String(describing: p3))
+  }
   /// %1$@ created the room
   internal static func stateEventRoomCreated(_ p1: Any) -> String {
     return L10n.tr("Localizable", "state_event_room_created", String(describing: p1))
@@ -2789,6 +2797,14 @@ internal enum L10n {
   /// You removed %1$@
   internal static func stateEventRoomRemoveByYou(_ p1: Any) -> String {
     return L10n.tr("Localizable", "state_event_room_remove_by_you", String(describing: p1))
+  }
+  /// You removed %1$@: %2$@
+  internal static func stateEventRoomRemoveByYouWithReason(_ p1: Any, _ p2: Any) -> String {
+    return L10n.tr("Localizable", "state_event_room_remove_by_you_with_reason", String(describing: p1), String(describing: p2))
+  }
+  /// %1$@ removed %2$@: %3$@
+  internal static func stateEventRoomRemoveWithReason(_ p1: Any, _ p2: Any, _ p3: Any) -> String {
+    return L10n.tr("Localizable", "state_event_room_remove_with_reason", String(describing: p1), String(describing: p2), String(describing: p3))
   }
   /// %1$@ sent an invitation to %2$@ to join the room
   internal static func stateEventRoomThirdPartyInvite(_ p1: Any, _ p2: Any) -> String {
