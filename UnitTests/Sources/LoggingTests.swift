248e88e718e1bac48c5b80bca5102ea1e3fa5dff
Mauro Romito <mauro.romito@element.io>
2025-06-04 16:33:27 +0200

    fixed tests

--- UnitTests/Sources/LoggingTests.swift
+++ UnitTests/Sources/LoggingTests.swift
@@ -96,7 +96,8 @@ class LoggingTests: XCTestCase {
                                       alternativeAliases: [],
                                       hasOngoingCall: false,
                                       isMarkedUnread: false,
-                                      isFavourite: false)
+                                      isFavourite: false,
+                                      isTombstoned: false)
         
         // When logging that value
         MXLog.info(roomSummary)
