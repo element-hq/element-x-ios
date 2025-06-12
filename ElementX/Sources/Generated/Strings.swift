23c63e8003918fa0672b40c888644898a6ad8a1a
Mauro Romito <mauro.romito@element.io>
2025-06-12 16:37:45 +0200

    add state to the accessibility label of rte formatting buttons

--- ElementX/Sources/Generated/Strings.swift
+++ ElementX/Sources/Generated/Strings.swift
@@ -512,6 +512,10 @@ internal enum L10n {
   internal static var commonRecoveryKey: String { return L10n.tr("Localizable", "common_recovery_key") }
   /// Refreshing…
   internal static var commonRefreshing: String { return L10n.tr("Localizable", "common_refreshing") }
+  /// %1$d replies
+  internal static func commonReplies(_ p1: Int) -> String {
+    return L10n.tr("Localizable", "common_replies", p1)
+  }
   /// Replying to %1$@
   internal static func commonReplyingTo(_ p1: Any) -> String {
     return L10n.tr("Localizable", "common_replying_to", String(describing: p1))
@@ -764,6 +768,8 @@ internal enum L10n {
   internal static var errorSomeMessagesHaveNotBeenSent: String { return L10n.tr("Localizable", "error_some_messages_have_not_been_sent") }
   /// Sorry, an error occurred
   internal static var errorUnknown: String { return L10n.tr("Localizable", "error_unknown") }
+  /// The sender of the event does not match the owner of the device that sent it.
+  internal static var eventShieldMismatchedSender: String { return L10n.tr("Localizable", "event_shield_mismatched_sender") }
   /// The authenticity of this encrypted message can't be guaranteed on this device.
   internal static var eventShieldReasonAuthenticityNotGuaranteed: String { return L10n.tr("Localizable", "event_shield_reason_authenticity_not_guaranteed") }
   /// Encrypted by a previously-verified user.
@@ -924,10 +930,20 @@ internal enum L10n {
   internal static var richTextEditorCreateLink: String { return L10n.tr("Localizable", "rich_text_editor_create_link") }
   /// Edit link
   internal static var richTextEditorEditLink: String { return L10n.tr("Localizable", "rich_text_editor_edit_link") }
+  /// %1$@, state: %2$@
+  internal static func richTextEditorFormatAction(_ p1: Any, _ p2: Any) -> String {
+    return L10n.tr("Localizable", "rich_text_editor_format_action", String(describing: p1), String(describing: p2))
+  }
   /// Apply bold format
   internal static var richTextEditorFormatBold: String { return L10n.tr("Localizable", "rich_text_editor_format_bold") }
   /// Apply italic format
   internal static var richTextEditorFormatItalic: String { return L10n.tr("Localizable", "rich_text_editor_format_italic") }
+  /// disabled
+  internal static var richTextEditorFormatStateDisabled: String { return L10n.tr("Localizable", "rich_text_editor_format_state_disabled") }
+  /// off
+  internal static var richTextEditorFormatStateOff: String { return L10n.tr("Localizable", "rich_text_editor_format_state_off") }
+  /// on
+  internal static var richTextEditorFormatStateOn: String { return L10n.tr("Localizable", "rich_text_editor_format_state_on") }
   /// Apply strikethrough format
   internal static var richTextEditorFormatStrikethrough: String { return L10n.tr("Localizable", "rich_text_editor_format_strikethrough") }
   /// Apply underline format
