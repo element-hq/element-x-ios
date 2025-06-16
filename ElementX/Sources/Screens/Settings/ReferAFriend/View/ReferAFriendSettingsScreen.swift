ca41d03436f7fc4f5dd9c26693b810e9dcf69ab5
shahroz-wilder <shrozali@outlook.com>
2025-06-16 17:45:58 +0500

    fix: updated refer a friend screen UI

--- ElementX/Sources/Screens/Settings/ReferAFriend/View/ReferAFriendSettingsScreen.swift
+++ ElementX/Sources/Screens/Settings/ReferAFriend/View/ReferAFriendSettingsScreen.swift
@@ -11,70 +11,66 @@ import SwiftUI
 struct ReferAFriendSettingsScreen: View {
     @ObservedObject var context: InviteFriendSettingsScreenViewModel.Context
     
+    @State private var referralCodeCopied: Bool = false
+    
     var body: some View {
         Form {
             ZeroListRow(kind: .custom {
-                ZStack {
-                    VStack(alignment: .center, spacing: 0) {
-                        Image(asset: Asset.Images.referAFriendImage)
+                VStack(alignment: .center, spacing: 0) {
+                    Image(asset: Asset.Images.referAFriendImage)
+                    
+                    VStack(spacing: 0) {
+                        Text("Refer a Friend")
+                            .font(.compound.headingMDBold)
+                            .foregroundColor(.zero.bgAccentRest)
                         
-                        VStack(spacing: 0) {
-                            Text("Refer a Friend")
-                                .font(.compound.headingMDBold)
+                        Text("Earn 30% of pro subs from your code.")
+                            .font(.zero.bodyMD)
+                            .foregroundColor(.compound.textPrimary)
+                        
+                        VStack {
+                            ReferAFriendBenefitRow(title: "Receive a Bonus",
+                                                   description: "Both you and the invitee get free tokens")
+                            
+                            ReferAFriendBenefitRow(title: "Earn Passive Income",
+                                                   description: "Receive 30% of Pro subscription revenue")
+                            
+                            ReferAFriendBenefitRow(title: "Grow your Reputation",
+                                                   description: "Increase your clout with a bigger network")
+                        }
+                        .padding(.top, 12)
+                        
+                        if context.viewState.inviteSlug.isEmpty || !context.viewState.hasRemaniningInvites {
+                            Text("Thank you! You’ve used all of your available invites. We’ll let you know when you can invite more people.")
+                                .font(.zero.bodyLG)
                                 .foregroundColor(.zero.bgAccentRest)
+                                .padding(16)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .fill(.zero.bgAccentRest.opacity(0.15))
+                                )
+                                .padding(.vertical, 10)
+                        } else {
+                            referralCodeLabel
+                                .padding(.vertical, 6)
                             
-                            Text("Earn 30% of pro subs from your code.")
-                                .font(.zero.bodyMD)
-                                .foregroundColor(.compound.textPrimary)
-                            
-                            VStack {
-                                ReferAFriendBenefitRow(title: "Earn Daily Income",
-                                                       description: "Only Pro members receive rewards")
-                                
-                                ReferAFriendBenefitRow(title: "Pro Badge",
-                                                       description: "Flex with a special Pro badge")
-                                
-                                ReferAFriendBenefitRow(title: "Earn Affiliate Fees",
-                                                       description: "Earn 30% of subscriptions from your code")
+                            HStack(spacing: 12) {
+                                BottomInfoBox(title: "Total invited so far",
+                                              description: context.viewState.totalInvited)
+                                BottomInfoBox(title: "Pro subs", description: "0")
                             }
-                            .padding(.top, 12)
                             
-                            if context.viewState.inviteSlug.isEmpty {
-                                Text("Thank you! You’ve used all of your available invites. We’ll let you know when you can invite more people.")
-                                    .font(.zero.bodyLG)
-                                    .foregroundColor(.zero.bgAccentRest)
-                                    .padding(16)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 12)
-                                            .fill(.zero.bgAccentRest.opacity(0.15))
-                                    )
-                                    .padding(.top, 10)
-                            } else {
-                                referralCodeLabel
-                                    .padding(.vertical, 6)
-                                
-                                referralCodeStrip
-                                
-                                HStack(spacing: 12) {
-                                    BottomInfoBox(title: "Total invited so far",
-                                                  description: context.viewState.totalInvited)
-                                    BottomInfoBox(title: "Pro subs", description: "0")
-                                }
+                            referralCodeStrip
                                 .padding(.top, 8)
-                                
-                                if context.viewState.hasRemaniningInvites {
-                                    shareInviteButton
-                                        .padding(.vertical, 10)
-                                }
+                            
+                            if context.viewState.hasRemaniningInvites {
+                                shareInviteButton
+                                    .padding(.vertical, 10)
                             }
                         }
                     }
-                    .frame(maxWidth: .infinity)
-                    
-                    if context.viewState.bindings.inviteCopied {
-                        referralCodeCopiedView
-                    }
                 }
+                .frame(maxWidth: .infinity)
             })
         }
         .ignoresSafeArea()
@@ -96,33 +92,49 @@ struct ReferAFriendSettingsScreen: View {
     }
     
     private var referralCodeStrip: some View {
-        HStack {
-            Text(context.viewState.inviteSlug)
-                .font(.zero.bodyLG)
-                .foregroundColor(.compound.textPrimary)
+        ZStack(alignment: .trailing) {
+            Text(referralCodeCopied ? "Text Copied!" : context.viewState.inviteSlug)
+                .font(.compound.bodyLGSemibold)
+                .foregroundColor(.zero.bgAccentRest)
                 .onTapGesture {
-                    context.send(viewAction: .inviteCopied)
+                    copyRefferalCode()
                 }
-            
-            Spacer()
-            
-            if context.viewState.hasRemaniningInvites {
-                Button(action: { context.send(viewAction: .inviteCopied) }) {
-                    Image(asset: Asset.Images.iconCopy)
-                        .foregroundStyle(.zero.bgAccentRest)
-                }
+                .frame(maxWidth: .infinity)
+                        
+            Button(action: { copyRefferalCode() }) {
+                Image(asset: referralCodeCopied ? Asset.Images.checkIcon : Asset.Images.iconCopy)
+                    .renderingMode(.template)
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .background(.zero.bgAccentRest.opacity(0.1))
+                    .foregroundStyle(.zero.bgAccentRest)
             }
         }
+        .frame(maxWidth: .infinity)
         .padding(14)
         .clipShape(RoundedRectangle(cornerRadius: 12))
+        .background(.zero.bgAccentRest.opacity(0.1))
         .overlay {
             RoundedRectangle(cornerRadius: 12)
                 .stroke(.zero.bgAccentRest.opacity(0.3), lineWidth: 1)
         }
+        .onTapGesture {
+            copyRefferalCode()
+        }
+    }
+    
+    func copyRefferalCode() {
+        if !referralCodeCopied {
+            referralCodeCopied = true
+            context.send(viewAction: .inviteCopied)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                referralCodeCopied = false
+            }
+        }
     }
     
     var shareInviteButton: some View {
-        Button(action: { context.send(viewAction: .inviteCopied) }) {
+        Button(action: { copyRefferalCode() }) {
             Text("Share Invite")
                 .font(.compound.bodyMDSemibold)
                 .foregroundColor(.black)
@@ -214,7 +226,7 @@ private struct BottomInfoBox: View {
                 .foregroundColor(.zero.bgAccentRest)
         }
         .frame(maxWidth: .infinity)
-        .padding(.vertical, 6)
+        .padding(.vertical, 5)
         .clipShape(RoundedRectangle(cornerRadius: 12))
         .overlay {
             RoundedRectangle(cornerRadius: 12)
