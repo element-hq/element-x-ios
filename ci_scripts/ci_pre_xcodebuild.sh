78d9ca37daaa278d97b3a21e85606f5a7fc59e2a
shahroz-wilder <shrozali@outlook.com>
2025-06-18 19:35:09 +0500

    chore: updated the script to generate google service from environment variables

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -8,8 +8,15 @@ cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"WALLET_CONNECT_PROJECT_ID\":\"%s\"}" "$WALLET_CONNECT_PROJECT_ID" >> ../Zero/SupportingFiles/zero_secrets.json
+echo "✅ Wrote zero_secrets.json file."
 
-echo "Wrote zero_secrets.json file."
+# Decode and write GoogleService-Info.plist
+if [ -n "$GOOGLE_SERVICE_INFO_BASE64" ]; then
+  echo "$GOOGLE_SERVICE_INFO_BASE64" | base64 --decode > ../Zero/SupportingFiles/GoogleService-Info.plist
+  echo "✅ Wrote GoogleService-Info.plist file."
+else
+  echo "⚠️ GOOGLE_SERVICE_INFO_BASE64 is not set. Skipping plist generation."
+fi
 
 echo "Stage: PRE-Xcode Build is DONE .... "
 
