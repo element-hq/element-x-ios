506231a592a19ed255c99bab10a1161b6179d35b
Mauro Romito <mauro.romito@element.io>
2025-07-16 17:54:49 +0200

    improved the tests, by filtering out some noise

--- AccessibilityTests/Sources/AccessibilityTests.swift
+++ AccessibilityTests/Sources/AccessibilityTests.swift
@@ -34,21 +34,45 @@ final class AccessibilityTests: XCTestCase {
             case .accessibilityAudit(let auditSignal):
                 switch auditSignal {
                 case .nextPreviewReady(let name):
-                    try? app.performAccessibilityAudit { issue in
-                        XCTFail("\(name): \(issue)")
-                        return true
+                    // Alows us to log the name of the preview that is being tested
+                    XCTContext.runActivity(named: name) { _ in
+                        do {
+                            // We have removed `textClipping` and `contrast` for now
+                            try app.performAccessibilityAudit(for: [.dynamicType, .elementDetection, .hitRegion, .sufficientElementDescription, .trait]) { issue in
+                                // Removew false positives for null elements
+                                guard let element = issue.element else {
+                                    return true
+                                }
+                                
+                                // Filter out false positives for specific cases
+                                if AccessibilityTests.ignoredA11yIdentifiers[element.identifier]?.contains(issue.auditType) == true {
+                                    return true
+                                }
+                                
+                                // We are fine with elements that only partially support dynamic types
+                                guard issue.compactDescription != "Dynamic Type font sizes are partially unsupported" else {
+                                    return true
+                                }
+                                
+                                return false
+                            }
+                        } catch {
+                            XCTFail("Failed to perform the accessibility audit: \(error)")
+                        }
                     }
                     try? client.send(.accessibilityAudit(.nextPreview))
                 case .noMorePreviews:
                     break forLoop
                 default:
-                    fatalError("Unhandled signal")
+                    XCTFail("Unhandled signal")
                 }
             default:
-                fatalError("Unhandled signal")
+                XCTFail("Unhandled signal")
             }
         }
         
         app.terminate()
     }
+    
+    private static let ignoredA11yIdentifiers: [String: [XCUIAccessibilityAuditType]] = [A11yIdentifiers.authenticationStartScreen.appVersion: [.hitRegion]]
 }
