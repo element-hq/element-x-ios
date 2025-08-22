4cb64e83eb6d34d4ef153dbbc3723e9e73b39701
Amsha Kalra <amshak@element.io>
2025-08-22 12:19:32 +0100

    Made the room filters smaller

--- ElementX/Sources/Screens/HomeScreen/View/Filters/RoomListFilterView.swift
+++ ElementX/Sources/Screens/HomeScreen/View/Filters/RoomListFilterView.swift
@@ -35,10 +35,10 @@ private struct FilterToggleStyle: ToggleStyle {
     func makeBody(configuration: Configuration) -> some View {
         let shape = RoundedRectangle(cornerRadius: 20)
         configuration.label
-            .font(.compound.bodyLG)
+            .font(.compound.bodyMD)
             .foregroundColor(foregroundColor(isOn: configuration.isOn))
-            .padding(.horizontal, 16)
-            .padding(.vertical, 8)
+            .padding(.horizontal, 12)
+            .padding(.vertical, 7)
             .background(shape.fill(backgroundColor(isOn: configuration.isOn)))
             .overlay {
                 shape
