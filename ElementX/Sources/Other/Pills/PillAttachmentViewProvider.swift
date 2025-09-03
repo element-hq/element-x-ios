e33631affa17c29df347f7ae2d11d6068f5c87e7
Doug <douglase@element.io>
2025-09-03 16:33:51 +0100

    Remove the unused media provider from the PillView.

--- ElementX/Sources/Other/Pills/PillAttachmentViewProvider.swift
+++ ElementX/Sources/Other/Pills/PillAttachmentViewProvider.swift
@@ -42,20 +42,17 @@ final class PillAttachmentViewProvider: NSTextAttachmentViewProvider, NSSecureCo
         }
         
         let context: PillContext
-        let mediaProvider: MediaProviderProtocol?
         if ProcessInfo.isXcodePreview || ProcessInfo.isRunningTests {
             // The mock viewModel simulates the loading logic for testing purposes
             context = PillContext.mock(viewState: .mention(isOwnMention: false, displayText: "Alice"), delay: .seconds(2))
-            mediaProvider = MediaProviderMock(configuration: .init())
         } else if let timelineContext = delegate?.timelineContext {
             context = PillContext(timelineContext: timelineContext, data: pillData)
-            mediaProvider = timelineContext.mediaProvider
         } else {
             MXLog.failure("Missing room context")
             return
         }
         
-        let view = PillView(mediaProvider: mediaProvider, context: context) { [weak self] in
+        let view = PillView(context: context) { [weak self] in
             self?.delegate?.invalidateTextAttachmentsDisplay()
         }
         let controller = UIHostingController(rootView: view)
