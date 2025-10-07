4c4914591ae65ccd377b6fb2f7b84d0b36ada0fd
Mauro Romito <mauro.romito@element.io>
2025-10-07 17:48:00 +0200

    updated preview tests

--- ElementX/Sources/Screens/LabsScreen/View/LabsScreen.swift
+++ ElementX/Sources/Screens/LabsScreen/View/LabsScreen.swift
@@ -63,7 +63,10 @@ struct LabsScreen: View {
 // MARK: - Previews
 
 struct LabsScreen_Previews: PreviewProvider, TestablePreview {
-    static let viewModel = LabsScreenViewModel(labsOptions: AppSettings())
+    static let viewModel = {
+        AppSettings.resetAllSettings()
+        return LabsScreenViewModel(labsOptions: AppSettings())
+    }()
     
     static var previews: some View {
         NavigationStack {
