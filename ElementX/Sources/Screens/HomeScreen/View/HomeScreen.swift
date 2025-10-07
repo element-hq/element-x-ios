51f69c33657c5acfa96b9db71c909fa903a202f3
Doug <douglase@element.io>
2025-10-07 10:38:46 +0100

    Make the space list bloom height match the room list.

--- ElementX/Sources/Screens/HomeScreen/View/HomeScreen.swift
+++ ElementX/Sources/Screens/HomeScreen/View/HomeScreen.swift
@@ -25,7 +25,7 @@ struct HomeScreen: View {
             .toolbar { toolbar }
             .background(Color.compound.bgCanvasDefault.ignoresSafeArea())
             .track(screen: .Home)
-            .bloom()
+            .toolbarBloom(hasSearchBar: true)
             .sentryTrace("\(Self.self)")
     }
     
