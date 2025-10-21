343f8efa7ac4550c4baeb03d317df771d700c34a
Mauro Romito <mauro.romito@element.io>
2025-10-21 14:26:44 +0200

    update copyrights done

--- ElementX/Sources/Other/Logging/MXLog.swift
+++ ElementX/Sources/Other/Logging/MXLog.swift
@@ -1,7 +1,6 @@
 //
 // Copyright 2025 Element Creations Ltd.
 // Copyright 2024-2025 New Vector Ltd.
-// Copyright 2025 Element Creations Ltd.
 // Copyright 2021-2025 The Matrix.org Foundation C.I.C
 //
 // SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
