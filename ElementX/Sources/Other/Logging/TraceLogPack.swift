3f87b51e4c451532a20b98dfc35fcc515b9af1d6
Doug <6060466+pixlwave@users.noreply.github.com>
2025-12-10 15:19:10 +0000

    Fix a bug where removing reactions would wipe out the last message in the room list. (#4834)
    Update the SDK.

--- ElementX/Sources/Other/Logging/TraceLogPack.swift
+++ ElementX/Sources/Other/Logging/TraceLogPack.swift
@@ -10,7 +10,7 @@ import Foundation
 import MatrixRustSDK
 
 enum TraceLogPack: Codable, CaseIterable {
-    case eventCache, sendQueue, timeline, notificationClient, syncProfiling
+    case eventCache, sendQueue, timeline, notificationClient, syncProfiling, latestEvents
     
     var title: String {
         switch self {
@@ -19,6 +19,7 @@ enum TraceLogPack: Codable, CaseIterable {
         case .timeline: "Timeline"
         case .notificationClient: "Notification client"
         case .syncProfiling: "Sync profiling"
+        case .latestEvents: "Latest events"
         }
     }
 }
@@ -32,6 +33,7 @@ extension TraceLogPack {
         case .timeline: self = .timeline
         case .notificationClient: self = .notificationClient
         case .syncProfiling: self = .syncProfiling
+        case .latestEvents: self = .latestEvents
         }
     }
     
@@ -42,6 +44,7 @@ extension TraceLogPack {
         case .timeline: .timeline
         case .notificationClient: .notificationClient
         case .syncProfiling: .syncProfiling
+        case .latestEvents: .latestEvents
         }
     }
 }
