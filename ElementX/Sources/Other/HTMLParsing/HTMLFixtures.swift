40b655bde6d14901c72d1d9d80196ad2cc8a91b7
Stefan Ceriu <stefanc@matrix.org>
2026-01-20 16:44:56 +0200

    Fix #4856, fix #4431 - Normalise HTML whitespaces using SwiftSoup's own text()/TextNode.normaliseWhitespace methods

--- ElementX/Sources/Other/HTMLParsing/HTMLFixtures.swift
+++ ElementX/Sources/Other/HTMLParsing/HTMLFixtures.swift
@@ -29,11 +29,11 @@ enum HTMLFixtures: String, CaseIterable {
             """
         case .headers:
             """
-            <h1>H1 Header</h1>
-            <h2>H2 Header</h2>
-            <h3>H3 Header</h3>
-            <h4>H4 Header</h4>
-            <h5>H5 Header</h5>
+            <h1>H1 Header</h1>\
+            <h2>H2 Header</h2>\
+            <h3>H3 Header</h3>\
+            <h4>H4 Header</h4>\
+            <h5>H5 Header</h5>\
             <h6>H6 Header</h6>
             """
         case .paragraphs:
--- ElementX/Sources/Other/Pills/MessageText.swift
+++ ElementX/Sources/Other/Pills/MessageText.swift
@@ -199,11 +199,10 @@ struct MessageText_Previews: PreviewProvider, TestablePreview {
 
     private static let htmlStringWithQuote =
         """
-        <blockquote>A blockquote that is long and goes onto multiple lines as the first item in the message</blockquote>
-        <p>Then another line of text here to reply to the blockquote, which is also a multiline component.</p>
+        <blockquote>A blockquote that is long and goes onto multiple lines as the first item in the message</blockquote><p>Then another line of text here to reply to the blockquote, which is also a multiline component.</p>
         """
     
-    private static let htmlStringWithList = "<p>This is a list</p>\n<ul>\n<li>One</li>\n<li>Two</li>\n<li>And number 3</li>\n</ul>\n"
+    private static let htmlStringWithList = "<p>This is a list</p>\n<ul><li>One</li>\n<li>Two</li>\n<li>And number 3</li>\n</ul>\n"
 
     private static let attributedStringBuilder = AttributedStringBuilder(mentionBuilder: MentionBuilder())
     
