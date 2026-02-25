b7bff0a29effce4367127af45f0c771c187f7322
Aaron Thornburgh <112560169+americanrefugee@users.noreply.github.com>
2026-02-25 17:24:10 +0100

    Update join call button in room header (#5143)
    * Change the Join Call button to 1.) show a Video Call Solid icon, 2.) change the label from "Join call" to "Join", and 3.) change the background color to primary/rest.
    
    * Add a clarifying comment and snapshots.

--- ElementX/Sources/Other/SwiftUI/Views/JoinCallButton.swift
+++ ElementX/Sources/Other/SwiftUI/Views/JoinCallButton.swift
@@ -22,12 +22,19 @@ struct JoinCallButton: View {
     
     var glassButton: some View {
         Button(action: action) {
-            Text(L10n.a11yJoinCall)
-                .font(.compound.bodyLG.weight(.medium))
-                .foregroundStyle(.compound.textOnSolidPrimary)
+            // Use an HStack on iOS 26 as .labelStyle(.titleAndIcon) doesn't
+            // seem to have any effect on a label in the navigation bar 🤷‍♂️
+            HStack(spacing: 6) {
+                CompoundIcon(\.videoCallSolid)
+                Text(L10n.actionJoin)
+                    .padding(.trailing, 4)
+            }
+            .font(.compound.bodyLG.weight(.medium))
+            .foregroundStyle(.compound.textOnSolidPrimary)
         }
-        .tint(.compound.bgAccentRest)
+        .tint(.compound.bgActionPrimaryRest)
         .backportButtonStyleGlassProminent()
+        .accessibilityLabel(L10n.a11yJoinCall)
     }
     
     var customButton: some View {
