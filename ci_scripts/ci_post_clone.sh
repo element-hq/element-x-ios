1bcd65dca3a283005584370be874a72eb82945ab
Stefan Ceriu <stefanc@matrix.org>
2026-03-02 12:34:24 +0200

    Fix xcode cloud nightly run build number setting

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -7,7 +7,7 @@ setup_xcode_cloud_environment
 install_xcode_cloud_brew_dependencies
 
 if [ "$CI_WORKFLOW" = "Nightly" ]; then
-    swift run tools ci configure-nightly
+    swift run tools ci configure-nightly --build-number $CI_BUILD_NUMBER
 elif [ "$CI_WORKFLOW" = "Element Pro" ]; then
     # Xcode Cloud automatically fetches the submodules.
     swift run pipeline configure-element-pro
