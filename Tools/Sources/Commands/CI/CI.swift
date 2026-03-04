2d5da1a62e19b4a82355c98440a48494e06adf95
Stefan Ceriu <stefanc@matrix.org>
2026-03-04 13:46:44 +0200

    Print out all the commands going through CI.Run and their arguments

--- Tools/Sources/Commands/CI/CI.swift
+++ Tools/Sources/Commands/CI/CI.swift
@@ -108,6 +108,8 @@ struct CI: ParsableCommand {
                                                                         environment: Environment = .inherit,
                                                                         output: Output = .standardOutput,
                                                                         error: Error = .standardError) async throws -> CollectedResult<Output, Error> {
+        logger.info("Running \(executable), with arguments: \(arguments)")
+        
         let result = try await Subprocess.run(executable,
                                               arguments: arguments,
                                               environment: environment,
