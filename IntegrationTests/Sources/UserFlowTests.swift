962d8133567fbf360340c88782fd36b9468e4826
manuroe <manu@matrix.org>
2026-04-08 10:32:13 +0200

    Integration tests: Remove sleep(30) in checkRoomFlows()
    Because https://github.com/element-hq/element-x-ios/issues/3365 is now closed. On the local simulator, I can see the test room appearing in the search result after some time, confirming the fix.
    
    Added an additional `XCTAssertTrue()` to help debugging CI failure.

--- IntegrationTests/Sources/UserFlowTests.swift
+++ IntegrationTests/Sources/UserFlowTests.swift
@@ -32,13 +32,9 @@ class UserFlowTests: XCTestCase {
     
     /// Assumes app is on the home screen
     private func checkRoomFlows() {
-        // Wait for the room list to paginate and correctly compute the room display names otherwise the test room
-        // won't be found
-        // Remove once https://github.com/element-hq/element-x-ios/issues/3365 gets sorted
-        sleep(30)
-        
         // Search for the special test room
         let searchField = app.searchFields.firstMatch
+        XCTAssertTrue(searchField.waitForExistence(timeout: 10.0))
         searchField.clearAndTypeText(Self.integrationTestsRoomName, app: app)
         
         // And open it
