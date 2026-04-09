4d5ca4b6e01571e2e65959bfd3043c9309376666
Mauro Romito <mauro.romito@element.io>
2026-04-09 22:19:46 +0200

    Live Location Sharing Banner
    # Conflicts:
    #	ElementX.xcodeproj/project.pbxproj

---
+++ ElementX/Sources/Screens/RoomScreen/View/LiveLocationSharingBannerView.swift
@@ -0,0 +1,42 @@
+//
+// Copyright 2025 Element Creations Ltd.
+//
+// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+// Please see LICENSE files in the repository root for full details.
+//
+
+import Compound
+import SwiftUI
+
+struct LiveLocationSharingBannerView: View {
+    var onStop: () -> Void
+    
+    var body: some View {
+        HStack(spacing: 9) {
+            CompoundIcon(\.locationPinSolid, size: .medium, relativeTo: .compound.bodyMDSemibold)
+                .foregroundColor(Color.compound.iconSuccessPrimary)
+                .accessibilityHidden(true)
+            Text(L10n.screenRoomLiveLocationBanner)
+                .font(.compound.bodyMDSemibold)
+                .foregroundColor(.compound.textPrimary)
+            Spacer()
+            Button(L10n.actionStop, role: .destructive) {
+                onStop()
+            }
+            .buttonStyle(.compound(.primary, size: .small))
+        }
+        .padding(.vertical, 16)
+        .padding(.horizontal, 15)
+        .background(Color.compound.bgCanvasDefault)
+        .overlay(Rectangle()
+            .inset(by: 0.5)
+            .stroke(.compound.separatorPrimary))
+    }
+}
+
+struct LiveLocationSharingBannerView_Previews: PreviewProvider, TestablePreview {
+    static var previews: some View {
+        LiveLocationSharingBannerView { }
+            .previewLayout(.sizeThatFits)
+    }
+}
