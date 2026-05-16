66eed14abcc735443678e09ccb697609e1a9afac
mredig <mredig@gmail.com>
2026-05-15 22:36:53 -0500

    Add SendableBox and WeakSendableBox

---
+++ ElementX/Sources/Other/SendableConformance/WeakSendableBox.swift
@@ -0,0 +1,33 @@
+//
+// Copyright 2026 Element Creations Ltd.
+//
+// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+// Please see LICENSE files in the repository root for full details.
+//
+
+import Foundation
+
+/// See ``Sendable`` - Everything is the same, but the value stored with `weak` reference semantics
+@dynamicMemberLookup
+final class WeakSendableBox<Wrapped: AnyObject>: @unchecked Sendable {
+    private let isolationLock = NSLock()
+    
+    private weak var _value: Wrapped?
+    var value: Wrapped? {
+        get { isolationLock.withLock { _value } }
+        set { isolationLock.withLock { _value = newValue } }
+    }
+    
+    subscript<T>(dynamicMember member: WritableKeyPath<Wrapped, T?>) -> T? {
+        get { value?[keyPath: member] }
+        set { value?[keyPath: member] = newValue }
+    }
+    
+    subscript<T>(dynamicMember member: KeyPath<Wrapped, T>) -> T? {
+        value?[keyPath: member]
+    }
+    
+    init(_ value: Wrapped) {
+        self._value = value
+    }
+}
