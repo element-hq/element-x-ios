5138e05e4795769ee015049bd180c618be1e103f
Doug <douglase@element.io>
2026-05-27 14:31:28 +0100

    Use SwiftFormat from develop and enabled indent-blank-lines option.

--- Tools/Sources/Commands/SetupProject.swift
+++ Tools/Sources/Commands/SetupProject.swift
@@ -17,7 +17,10 @@ struct SetupProject: ParsableCommand {
     }
 
     func brewInstall() throws {
-        try Zsh.run(command: "brew install xcodegen swiftgen swiftformat git-lfs sourcery mint pkl kiliankoe/formulae/swift-outdated localazy/tools/localazy peripheryapp/periphery/periphery")
+        try Zsh.run(command: "brew install xcodegen swiftgen git-lfs sourcery mint pkl kiliankoe/formulae/swift-outdated localazy/tools/localazy peripheryapp/periphery/periphery")
+        
+        // Install swiftformat from develop (for now), making sure to avoid conflicts with an existing versioned installation. 
+        try Zsh.run(command: "source ci_scripts/ci_common.sh && install_swiftformat_head")
     }
 
     func mintPackagesInstall() throws {
