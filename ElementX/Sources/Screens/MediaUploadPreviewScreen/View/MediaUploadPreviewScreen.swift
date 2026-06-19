8a4e4d1903fd11614cf8297b32ede00bdb7c6189
Mauro Romito <mauro.romito@element.io>
2026-06-19 12:52:19 +0200

    Give the media upload preview a descriptive VoiceOver title
    The QLPreviewItem used the media's file name as its title, which VoiceOver
    read as cryptic alt text. Provide a descriptive "Photo preview" / "Video
    preview" title based on the media type instead.
    
    Addresses #5240, #5241.

--- ElementX/Sources/Screens/MediaUploadPreviewScreen/View/MediaUploadPreviewScreen.swift
+++ ElementX/Sources/Screens/MediaUploadPreviewScreen/View/MediaUploadPreviewScreen.swift
@@ -264,7 +264,16 @@ private struct PreviewView: UIViewControllerRepresentable {
         }
         
         func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
-            PreviewItem(previewItemURL: view.mediaURLs[index], previewItemTitle: view.title)
+            let url = view.mediaURLs[index]
+            // Use a descriptive title instead of the file name so VoiceOver doesn't read a cryptic name.
+            return PreviewItem(previewItemURL: url, previewItemTitle: previewItemTitle(for: url))
+        }
+        
+        private func previewItemTitle(for url: URL) -> String? {
+            guard let type = UTType(filenameExtension: url.pathExtension) else { return view.title }
+            if type.conforms(to: .image) { return L10n.a11yPhotoPreview }
+            if type.conforms(to: .movie) || type.conforms(to: .video) { return L10n.a11yVideoPreview }
+            return view.title
         }
         
         // MARK: - QLPreviewControllerDelegate
