51985332420530e3a0fbb32979dffc36092f328e
Jürgen Wischer <juergen.wischer@bwi.de>
2026-07-14 22:38:20 +0200

    Add a visible custom focus ring to the standard button style

--- compound-ios/Sources/Compound/BaseStyles/CompoundButtonStyle.swift
+++ compound-ios/Sources/Compound/BaseStyles/CompoundButtonStyle.swift
@@ -22,6 +22,7 @@ public struct CompoundButtonStyle: ButtonStyle {
     @Environment(\.isEnabled) private var isEnabled
     @Environment(\.colorScheme) private var colorScheme
     @Environment(\.accessibilityShowButtonShapes) private var accessibilityShowButtonShapes
+    @Environment(\.isFocused) private var isFocused
     
     var kind: Kind
     public enum Kind {
@@ -117,6 +118,11 @@ public struct CompoundButtonStyle: ButtonStyle {
                 makeBackground(configuration: configuration)
             }
             .contentShape(contentShape)
+            .overlay {
+                if isFocused {
+                    contentShape.stroke(.compound.borderInteractivePrimary, lineWidth: 2)
+                }
+            }
     }
     
     @ViewBuilder
