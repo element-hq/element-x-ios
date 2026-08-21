69453295814057f5dcf09d903096aaa38a4d8081
Doug <douglase@element.io>
2026-08-21 11:33:39 +0100

    Update the command line tools and fix the Enterprise dependency.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import Foundation
 var dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.8.1")),
     .package(url: "https://github.com/element-hq/swift-command-line-tools.git", branch: "main"),
-    // .package(path: "../../../swift-command-line-tools"),
+    // .package(path: "../swift-command-line-tools"),
     .package(url: "https://github.com/swiftlang/swift-subprocess", .upToNextMinor(from: "0.3.0")),
     .package(url: "https://github.com/jpsim/Yams", .upToNextMinor(from: "6.2.2")),
     .package(url: "https://github.com/apple/swift-log", .upToNextMinor(from: "1.10.1")),
@@ -15,7 +15,7 @@ var dependencies: [Package.Dependency] = [
 ]
 
 if FileManager.default.fileExists(atPath: "Enterprise/Pipeline/Package.swift") {
-    dependencies.append(.package(path: "./enterprise/pipeline"))
+    dependencies.append(.package(path: "./Enterprise/Pipeline"))
 }
 
 let package = Package(
